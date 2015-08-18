5182e0e8d8d3f5219a60e5b1a941dfe35f2bbe20
wickning1 <git@mythgaming.net>
2015-08-18 10:42:05 -0500

    updated rebuild to symlink light modules for easier development, requires Context allowLinking="true" in tomcat/conf/Context.xml

--- rebuild.pl
+++ rebuild.pl
@@ -1,47 +1,62 @@
 #!/usr/bin/perl
 use strict;
+use Cwd;
 
-my $tomcatdir = "tomcat";
-my $superlight = $ARGV[0] eq '--super';
-my $light = $ARGV[0] eq '--light';
-my $fullrun = !$light && !$superlight;
-
-if ($fullrun) {
-	print "mvn clean install...\n";
-	my $output = `mvn clean install`;
-	if ($output =~ m/FAILURE/) { print $output."\n"; }
+my $gatodir = cwd();
+my $tomcatdir = $gatodir."/tomcat";
+my @lightmodules = ('gato-template', 'gato-template-tsus', 'gato-template-txstate2015');
+my $module = "";
+if ($ARGV[0] eq '--module') {
+	$module = $ARGV[1];
 }
 
-if ($light || $fullrun) {
-	print "stopping tomcat...\n";
-	`$tomcatdir/bin/shutdown.sh`;
+if ($module) {
+	print "building individual module $module...\n";
+	chdir($gatodir.'/'.$module);
+} else {
+	chdir($gatodir);
 }
 
-if ($fullrun) {
+print "mvn clean package...\n";
+my $output = `mvn clean package`;
+if ($output =~ m/FAILURE/) { print $output."\n"; }
+
+print "stopping tomcat...\n";
+`$tomcatdir/bin/shutdown.sh`;
+
+if (!$module) {
 	print "removing old webapp...\n";
 	`rm -rf $tomcatdir/webapps/ROOT*`;
 
 	print "copying war...\n";
-	`cp magnolia-project-webapp/target/magnolia-project-webapp*war $tomcatdir/webapps/ROOT.war`;
+	`cp $gatodir/gato-webapp/target/gato-webapp*war $tomcatdir/webapps/ROOT.war`;
+} else {
+	print "copying module jar to webapp...\n";
+	`rm $tomcatdir/webapps/ROOT/WEB-INF/lib/$module*.jar`;
+	`cp $gatodir/$module/target/$module*.jar $tomcatdir/webapps/ROOT/WEB-INF/lib/`;
+}
+
+print "starting tomcat...\n";
+`$tomcatdir/bin/startup.sh`;
+
+if (!$module) {
+	print "waiting to symlink light modules...\n";
+	my $tries = 0;
+	while (1) { 
+		sleep(1);
+		my $allpresent = 1;
+		foreach my $lm (@lightmodules) {
+			$allpresent = 0 unless -e "$tomcatdir/webapps/ROOT/$lm";
+		}
+		last if $allpresent || $tries++ > 60;
+	}
+	sleep(1); # just for good measure
 	
-	print "starting tomcat...\n";
-	`$tomcatdir/bin/startup.sh`;
-}
-
-if ($light || $superlight) {
-	print "removing light modules...\n";
-	`rm -rf $tomcatdir/webapps/ROOT/gato-template`;
-	`rm -rf $tomcatdir/webapps/ROOT/gato-template-tsus`;
-}
-
-while (!-e "$tomcatdir/webapps/ROOT") { }
-print "copying light modules...\n";
-`cp -R gato-template $tomcatdir/webapps/ROOT/gato-template`;
-`cp -R gato-template-tsus $tomcatdir/webapps/ROOT/gato-template-tsus`;
-
-if ($light) {
-	print "starting tomcat...\n";
-	`$tomcatdir/bin/startup.sh`;
+	print "symlinking light modules...\n";
+	`rm -rf $tomcatdir/webapps/ROOT/gato-*`;
+	foreach my $lm (@lightmodules) {
+		`ln -s $gatodir/$lm $tomcatdir/webapps/ROOT/$lm`;
+	}
 }
 
 print "Done.\n";
