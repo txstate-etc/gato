0e53445a0705cad1008b8b0e3f33127f4b180b8c
wickning1 <git@mythgaming.net>
2015-09-22 15:08:02 -0500

    major update to rebuild to handle a lot more situations, --resources to set up your system for symlinking like Charles described, untested --dry that depends on a pre-built war for the old build, --restart for a clean tomcat restart, also you may now create rebuild_vars.pl to store private paths and such, just override the global variables in rebuild with your own values

--- rebuild.pl
+++ rebuild.pl
@@ -4,59 +4,70 @@ use Cwd;
 
 our $gatodir = cwd();
 our $tomcatdir = $gatodir."/tomcat";
+our $urlbase = "http://localhost:8080";
+our $lastwarpath = "$gatodir/../magnolia-empty-webapp*war";
+our $edgewarpath = "$gatodir/gato-webapp/target/gato-webapp*war";
+our $magnoliapropertiespath = "/etc/magnolia/config";
 our @lightmodules = ('gato-template', 'gato-template-tsus', 'gato-template-txstate2015');
 our @heavymodules = ('gato-lib', 'gato-internal', 'gato-component-cssjs', 'gato-component-dept-directory', 
 	'gato-component-faq', 'gato-component-gallery');
 our @sassfiles = ('resources/gato-template-tsus/css/tsus-home.scss', 
 									'resources/gato-template-txstate2015/css/txstate2015.scss');
 our $module = "";
+
+# place your local overrides in the root gato directory in a file named "rebuild_vars.pl"
+# git will ignore the file
+if (-e "$gatodir/rebuild_vars.pl") { require("$gatodir/rebuild_vars.pl"); }
+
 if ($ARGV[0] eq '--module') {
 	$module = $ARGV[1];
-} elsif ($ARGV[0] eq '--light') {
+  buildmodule($module);
 	tomcat_restart(sub {
-		copy_light();
+	  replacemodule($module);
 	});
-	print "Done.\n";
-	exit;
-} elsif ($ARGV[0] eq '--sass') {
-	sass();
-	print "Done.\n";
-	exit;
-}
-
-if ($module) {
-	print "building individual module $module...\n";
-	chdir($gatodir.'/'.$module);
-} else {
-	chdir($gatodir);
-}
-
-print "mvn clean package...\n";
-my $output = `mvn clean package`;
-if ($output =~ m/FAILURE|ERROR/) { print $output."\n"; exit; }
-
-tomcat_restart(sub {
-	if (!$module) {
-		print "removing old webapp...\n";
-		`rm -rf $tomcatdir/webapps/ROOT*`;
-		print "copying war...\n";
-		`cp $gatodir/gato-webapp/target/gato-webapp*war $tomcatdir/webapps/ROOT.war`;
-	} else {
-		copy_light();
-		print "removing old $module jar...\n";
-		`rm $tomcatdir/webapps/ROOT/WEB-INF/lib/$module*.jar`;
-		print "copying new $module jar to webapp...\n";
-		`cp $gatodir/$module/target/$module*.jar $tomcatdir/webapps/ROOT/WEB-INF/lib/`;
+} elsif ($ARGV[0] eq '--restart') {
+  tomcat_restart();
+} elsif ($ARGV[0] eq '--resources') {
+	if (setmagnoliaresourcespath()) {
+  	tomcat_restart(sub {
+	  	symlinkheavyresources();
+	  });
 	}
-});
+} elsif ($ARGV[0] eq '--sass') {
+  symlinkheavyresources();
+	sass();
+} elsif ($ARGV[0] eq '--dry') {
+	tomcat_restart(sub {
+		resetdata();
+		cleanwebapp();
+		installwar($lastwarpath);
+	});
+	triggerbootstrap();
+	buildedge();
+	waitforbootstrap();
+	tomcat_restart(sub {
+	  cleanwebapp();
+	  installwar($edgewarpath);
+	});
+	triggerbootstrap();
+} else {
+  buildedge();
+  tomcat_restart(sub {
+    cleanwebapp();
+    installwar($edgewarpath);
+  });
+  triggerbootstrap();
+}
 
 print "Done.\n";
+
 
 sub tomcat_restart {
 	my $nested = shift;
 	
 	print "stopping tomcat...\n";
-	`$tomcatdir/bin/shutdown.sh`;
+	my $pid = `pgrep -f $tomcatdir`;
+	`kill $pid` if $pid > 0;
 
 	my $matchstr = quotemeta($tomcatdir);
 	while (`ps ax` =~ m/$matchstr/) {
@@ -67,33 +78,88 @@ sub tomcat_restart {
 	print "wiping out the cache directory so our CSS/JS gets reloaded...\n";
 	`rm -rf $tomcatdir/webapps/ROOT/cache`;
 	
-	$nested->();
+	$nested->() if $nested;
 	
 	print "starting tomcat...\n";
 	`$tomcatdir/bin/startup.sh`;
 }
 
-sub copy_light {
-	# print "copying light modules...\n";
-	# `rm -rf $tomcatdir/webapps/ROOT/gato-*`;
-	# foreach my $lm (@lightmodules) {
-	# 	`cp -R $gatodir/$lm $tomcatdir/webapps/ROOT/$lm`;
-	# }
-	
+sub setmagnoliaresourcespath() {
+  my $success = 0;
+  print "creating magnolia properties file to point resources at git repo...\n";
+  `mkdir -p $magnoliapropertiespath`;
+  open(my $fh, '>', "$magnoliapropertiespath/magnolia.properties") or warn $!;
+  if (-w $fh) {
+    print $fh "magnolia.resources.dir=$gatodir/resources";
+    $success = 1;
+  } else {
+    print "Need write permission, try sudo.\n";
+  }
+  close($fh);
+  return $success;
+}
+
+sub symlinkheavyresources {
+  print "symlinking heavy modules into $gatodir/resources...\n";
+  foreach my $hm (@heavymodules) {
+    `ln -s $gatodir/$hm/src/main/resources/$hm $gatodir/resources/$hm` unless -e "$gatodir/resources/$hm";
+  }
+}
+
+sub resetdata {
+	print "resetting all data...\n";
+	`mysql -u root -e "DROP DATABASE IF EXISTS magnolia; CREATE DATABASE magnolia`;
+	`rm /var/mag_repositories/magnolia`;
+}
+
+sub cleanwebapp {
+	print "removing old webapp...\n";
+	`rm -rf $tomcatdir/webapps/ROOT*`;
+}
+
+sub buildedge {
+  print "building full war...\n";
+  chdir($gatodir);
+  my $output = `mvn clean package`;
+  if ($output =~ m/FAILURE|ERROR/) { print $output."\n"; exit; }
+}
+
+sub installwar {
+	my $warpath = shift;
+  print "copying war...\n";
+  `cp $warpath $tomcatdir/webapps/ROOT.war`;
+}
+
+sub buildmodule {
+  my $module = shift;
+	print "building individual module $module...\n";
+	chdir($gatodir.'/'.$module);
+	`mvn install package`;
+}
+
+sub replacemodule {
+  my $module = shift;
+  print "removing old $module jar...\n";
+  `rm $tomcatdir/webapps/ROOT/WEB-INF/lib/$module*.jar`;
+  print "copying new $module jar to webapp...\n";
+  `cp $gatodir/$module/target/$module*.jar $tomcatdir/webapps/ROOT/WEB-INF/lib/`;
+}
+
+sub triggerbootstrap() {
+	print "Initiating upgrade tasks...\n";
+	sleep(10);
+	`curl $urlbase/.magnolia/installer > /dev/null`;
+	`curl $urlbase/.magnolia/installer/start > /dev/null`;	
+}
+
+sub waitforbootstrap() {
+  sleep(120);
 }
 
 # assumes `sass` is available on your system
 sub sass {
-	print "Compiling SASS files...\n";
-	my @loadpaths = ($gatodir.'/resources');
-	foreach my $hm (@heavymodules) {
-		push @loadpaths, $gatodir.'/'.$hm.'/src/main/resources';
-	}
-	
-	my $loadpaths = '';
-	foreach my $lp (@loadpaths) {
-		$loadpaths .= ' --load-path "'.$lp.'"';
-	}
+	print "Compiling SASS files...\n";	
+	my $loadpaths = '--load-path '.$gatodir.'/resources';
 	
 	foreach my $file (@sassfiles) {
 		my $input = "$gatodir/$file";
