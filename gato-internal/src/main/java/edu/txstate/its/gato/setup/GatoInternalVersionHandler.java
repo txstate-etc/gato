533ecc4a2ceb802b2f03ff68ff33acfa16d695d8
wickning1 <nwing@txstate.edu>
2015-09-25 16:14:44 -0500

    starting to add upgrade tasks to change template names

--- gato-internal/src/main/java/edu/txstate/its/gato/setup/GatoInternalVersionHandler.java
+++ gato-internal/src/main/java/edu/txstate/its/gato/setup/GatoInternalVersionHandler.java
@@ -4,7 +4,12 @@ import info.magnolia.module.DefaultModuleVersionHandler;
 import info.magnolia.module.delta.BootstrapSingleResource;
 import info.magnolia.module.delta.DeltaBuilder;
 import info.magnolia.module.delta.FindAndChangeTemplateIdTask;
+import info.magnolia.module.delta.Task;
+import info.magnolia.module.InstallContext;
 import info.magnolia.repository.RepositoryConstants;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class is optional and lets you manager the versions of your module,
@@ -15,9 +20,25 @@ public class GatoInternalVersionHandler extends DefaultModuleVersionHandler {
   public GatoInternalVersionHandler() {
     register(DeltaBuilder.update("1.0.1", "")
       .addTask(new BootstrapSingleResource("Bootstrap", "Bootstrap class definition for doing image resizing on Texas State imagehandlers server", "/mgnl-bootstrap/gato-internal/config.modules.gato-lib.imaging.resize.xml"))
-      .addTask(new Gato5MigrationTask("Gato Migrate to 5 task", "Generic update task for all the things we need to do to upgrade to Magnolia 5."))
-      .addTask(new FindAndChangeTemplateIdTask(RepositoryConstants.WEBSITE, 
-        "gato:components/texasState/texasEditor", "gato-template:components/richeditor"))
     );
   }
+  @Override
+  protected List<Task> getExtraInstallTasks(InstallContext installContext) {
+    List<Task> tasks = new ArrayList<Task>(super.getExtraInstallTasks(installContext));
+    tasks.add(new Gato5MigrationTask("Gato Migrate to 5 task", "Generic update task for all the things we need to do to upgrade to Magnolia 5."));
+    tasks.add(new FindAndChangeTemplateIdTask(RepositoryConstants.WEBSITE, 
+      "gato:components/texasState/customCssBlock", "gato-component-cssjs:components/customcss"));
+    tasks.add(new FindAndChangeTemplateIdTask(RepositoryConstants.WEBSITE, 
+      "gato:components/texasState/customjsBlock", "gato-component-cssjs:components/customjs"));
+    tasks.add(new FindAndChangeTemplateIdTask(RepositoryConstants.WEBSITE, 
+      "gato:components/texasState/texasEditor", "gato-template:components/richeditor"));
+    tasks.add(new FindAndChangeTemplateIdTask(RepositoryConstants.WEBSITE, 
+      "gato:pages/tsus-2012/tsus-2012-home", "gato-template-tsus:pages/home"));
+    tasks.add(new FindAndChangeTemplateIdTask(RepositoryConstants.WEBSITE, 
+      "gato:pages/tsus-2012/tsus-2012-mail", "gato-template-tsus:pages/mail"));
+    tasks.add(new FindAndChangeTemplateIdTask(RepositoryConstants.WEBSITE, 
+      "gato:pages/tsus-2012/tsus-2012-standard", "gato-template-tsus:pages/standard"));
+    return tasks;
+  }
+
 }
