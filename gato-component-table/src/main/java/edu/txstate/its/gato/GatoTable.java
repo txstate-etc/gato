8a25b6d9ba67865d4173043817cbb3cf93774733
Anne Faber <anne.faber@txstate.edu>
2015-10-06 13:59:39 -0500

    started table paragraph

---
+++ gato-component-table/src/main/java/edu/txstate/its/gato/GatoTable.java
@@ -0,0 +1,12 @@
+package edu.txstate.its.gato;
+
+/**
+ * This class is optional and represents the configuration for the gato-component-table module.
+ * By exposing simple getter/setter/adder methods, this bean can be configured via content2bean
+ * using the properties and node from <tt>config:/modules/gato-component-table</tt>.
+ * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ */
+public class GatoTable {
+    /* you can optionally implement info.magnolia.module.ModuleLifecycle */
+
+}
