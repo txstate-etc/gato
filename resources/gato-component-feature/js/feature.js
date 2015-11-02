8c3527ba880157d455a072538cffcfdbbdb192e4
Charles Jones <cj32@txstate.edu>
2015-11-01 22:17:08 -0600

    Implement feature paragraph auto-rotate.

--- resources/gato-component-feature/js/feature.js
+++ resources/gato-component-feature/js/feature.js
@@ -1,6 +1,11 @@
 jQuery(document).ready(function ($) {
-  $('.gato-feature-slides').slick({
-    dots: true,
-    adaptiveHeight: true
+  $('.gato-feature').each(function() {
+    var speed = $(this).attr('data-feature-timer');
+    $(this).find('.gato-feature-slides').slick({
+      dots: true,
+      adaptiveHeight: true,
+      autoplay: !isEditMode && (speed > 0),
+      autoplaySpeed: speed * 1000,
+    });
   });
 });
