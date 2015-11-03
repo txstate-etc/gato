cc055ad5987a9afc99c008092d10dd205c5fb663
Anne Faber <anne.faber@txstate.edu>
2015-11-03 16:18:01 -0600

    commenting out text-changing code until we know what the text should be

--- resources/gato-template/js/edit-bars.js
+++ resources/gato-template/js/edit-bars.js
@@ -1,7 +1,8 @@
 jQuery( document ).ready(function() {
     //need to wait for the bars to load.  they are slooooooooow
     //should use MutationObserver if javascript is necessary to modify the bars
-    setTimeout(function() {
-        jQuery('.mgnlPlaceholder .mgnlEditorBar .mgnlEditorBarLabel').html("+ Add Content");
-    }, 1000);
+    // setTimeout(function() {
+        
+    //     jQuery('.mgnlPlaceholder .mgnlEditorBar .mgnlEditorBarLabel').html("+ Add Content");
+    // }, 1000);
 });
