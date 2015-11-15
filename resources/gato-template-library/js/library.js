e3d5b4b986a634d6d2436ff0a7bdf580ad04ee5d
wickning1 <git@mythgaming.net>
2015-11-14 19:30:12 -0600

    more tools dropdown fix for library homepage #6810

---
+++ resources/gato-template-library/js/library.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function ($) {
+  // this line is duplicated in txstate2015's common.js
+  // probably need a refactoring since the templates share a header but nothing else
+  $('.more-tools > a').hovermenu('.super-list-sub');
+});
