19e041bcef3a3fc65ed9af262eb99ba4c858b29c
Charles Jones <cj32@txstate.edu>
2015-11-19 16:24:10 -0600

    Rewrote sort command to use Node API instead of old and busted Content API.

--- gato-lib/src/main/java/edu/txstate/its/gato/SortCommand.java
+++ gato-lib/src/main/java/edu/txstate/its/gato/SortCommand.java
@@ -1,25 +1,25 @@
 package edu.txstate.its.gato;
 
-import info.magnolia.cms.beans.config.ContentRepository;
-import info.magnolia.cms.core.Content;
-import info.magnolia.cms.core.HierarchyManager;
-import info.magnolia.cms.core.ItemType;
 import info.magnolia.context.Context;
 import info.magnolia.context.MgnlContext;
-// import info.magnolia.module.admininterface.commands.BaseRepositoryCommand;
+import info.magnolia.jcr.util.NodeTypes;
+import info.magnolia.jcr.util.NodeUtil;
+import info.magnolia.jcr.util.PropertyUtil;
 import java.util.Iterator;
 import java.util.Vector;
+import javax.jcr.Node;
 import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 
 public class SortCommand extends GatoBaseSchedulerCommand {
   
-  private boolean canSort(Content rootNode) {
+  private boolean canSort(Node rootNode) throws RepositoryException {
     Vector paths = new Vector();
     String duplicates = "";
 
-    Iterator childNodesIterator = rootNode.getChildren(ItemType.CONTENT).iterator();
+    Iterator childNodesIterator = NodeUtil.getNodes(rootNode, NodeTypes.Content.NAME).iterator();
     while (childNodesIterator.hasNext()) {
-      Content site = (Content) childNodesIterator.next();
+      Node site = (Node) childNodesIterator.next();
       String siteName = site.getName();
       if ( paths.contains( siteName ) ) {
         duplicates = duplicates + " " + siteName;
@@ -38,61 +38,47 @@ public class SortCommand extends GatoBaseSchedulerCommand {
   }
 
   public boolean execute(Context context) {
-    log.info("SortCommand called. But it doesn't work, so we're not doing anything...");
-    // log.info("SortCommand called. Getting Hierarchy Manager...");
+    log.info("SortCommand called. Getting JCR Session...");
 
-    // HierarchyManager hm = MgnlContext.getHierarchyManager(this.getRepository());
+    try {
+      Session session = MgnlContext.getJCRSession(this.getRepository());
 
-    // if (hm == null) {
-    //   log.error("Couldn't get Hiererchy Manager for repository: " + this.getRepository());
-    //   return false;
-    // }
+      log.debug("Getting root node...");
+      Node rootNode = session.getNode(this.getPath());
 
-    // log.debug("Getting root node...");
-    // Content rootNode;
-    // try {
-    //   rootNode = hm.getContent(this.getPath());
-    // } catch (RepositoryException ex) {
-    //   log.error("Couldn't get node: " + this.getPath());
-    //   return false;
-    // }
+      if (canSort(rootNode)) {
+        log.debug("Sorting nodes");
 
-    // if (canSort(rootNode)) {
-    //   log.debug("Sorting nodes");
+        boolean isSorted = false;
 
-    //   boolean isSorted = false;
-    //   int outer = 0;
+        while (!isSorted) {
+          isSorted = true;
+          Node previousSite = null;
 
-    //   while (!isSorted) {
-    //     log.debug("outer " + outer++);
-    //     Iterator childNodesIterator = rootNode.getChildren(ItemType.CONTENT).iterator();
+          Iterator childNodesIterator = NodeUtil.getNodes(rootNode, NodeTypes.Page.NAME).iterator();
+          while (childNodesIterator.hasNext()) {
+            Node site = (Node) childNodesIterator.next();
+            String title = PropertyUtil.getString(site, "title");
+            String prevTitle = PropertyUtil.getString(previousSite, "title");
+            if ((previousSite != null) && (title.compareToIgnoreCase(prevTitle) < 0)) {
+              log.debug("Moving " + title + " before " + prevTitle);
+              rootNode.orderBefore(site.getName(), previousSite.getName());
+              rootNode.save();
+              isSorted = false;
+            }
+            previousSite = site;
+          }
 
-    //     isSorted = true;
-    //     Content previousSite = null;
-    //     int inner = 0;
+        }
 
-    //     while (childNodesIterator.hasNext()) {
-    //       log.debug("inner " + inner++);
-    //       Content site = (Content) childNodesIterator.next();
-    //       log.debug("site " + site.getTitle());
-    //       if ((previousSite != null) && (site.getTitle().compareToIgnoreCase(previousSite.getTitle()) < 0)) {
-    //         try {
-    //           log.debug("Moving " + site.getTitle() + " before " + previousSite.getTitle());
-    //           rootNode.orderBefore(site.getName(), previousSite.getName());
-    //           rootNode.save();
-    //           isSorted = false;
-    //         } catch (RepositoryException ex) {
-    //           ex.printStackTrace();
-    //         }
-    //       }
-    //       previousSite = site;
-    //     }
+      }
 
-    //   }
+      log.info("SortCommand completed.");
+      return true;
 
-    // }
-
-    log.info("SortCommand completed.");
-    return true;
+    } catch (RepositoryException e) {
+      log.error("SortCommand failed for repository: {}", this.getRepository(), e);
+      return false;
+    }
   }
 }
