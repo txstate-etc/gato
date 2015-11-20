4912f0d7a3b4250715f5775e610184a9262cb282
wickning1 <git@mythgaming.net>
2015-11-20 00:10:48 -0600

    sidebarwrap.js no longer produces errors on pages without the sidebar

--- resources/gato-template-txstate2015/js/sidebarwrap.js
+++ resources/gato-template-txstate2015/js/sidebarwrap.js
@@ -2,22 +2,24 @@ jQuery(document).ready(function ($) {
   var sidebar = $('.page_content .sidebar');
   var pagecontent = $('.page_content');
   var footer = $('.footer');
-  // prevent sidebar from flowing behind the footer,
-  // since it is position: absolute
-  resizeTimeout(function() {
-    pagecontent.css('min-height', Math.max($(window).height()-pagecontent.offset().top-footer.height(), sidebar.height()+75)+'px');
-  });
-
-  // find the first gato-component paragraph whose top is
-  // below the sidebar and expand it to full width
-  resizeTimeout(function() {
-    $('.page_content .gato-component').each(function (i, cmp) {
-      cmp = $(cmp);
-      if (cmp.position().top > sidebar.position().top+sidebar.height()) {
-        cmp.css('width', '100%');
-      } else {
-        cmp.css('width', '');
-      }
+  if (sidebar.size() > 0) {
+    // prevent sidebar from flowing behind the footer,
+    // since it is position: absolute
+    resizeTimeout(function() {
+      pagecontent.css('min-height', Math.max($(window).height()-pagecontent.offset().top-footer.height(), sidebar.height()+75)+'px');
     });
-  });
+
+    // find the first gato-component paragraph whose top is
+    // below the sidebar and expand it to full width
+    resizeTimeout(function() {
+      $('.page_content .gato-component').each(function (i, cmp) {
+        cmp = $(cmp);
+        if (cmp.position().top > sidebar.position().top+sidebar.height()) {
+          cmp.css('width', '100%');
+        } else {
+          cmp.css('width', '');
+        }
+      });
+    });
+  }
 });
