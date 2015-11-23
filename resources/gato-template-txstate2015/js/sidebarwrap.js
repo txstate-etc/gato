efc1e87303f89e7a39a91a801eb5dc727be9ceff
wickning1 <nwing@txstate.edu>
2015-11-23 16:30:50 -0600

    make sure to set min-height on the 2015 body again after edit bars have loaded, because they will increase the needed height #7011

--- resources/gato-template-txstate2015/js/sidebarwrap.js
+++ resources/gato-template-txstate2015/js/sidebarwrap.js
@@ -5,9 +5,12 @@ jQuery(document).ready(function ($) {
   if (sidebar.size() > 0) {
     // prevent sidebar from flowing behind the footer,
     // since it is position: absolute
-    resizeTimeout(function() {
+    var setsidebarminheight = function() {
       pagecontent.css('min-height', Math.max($(window).height()-pagecontent.offset().top-footer.height(), sidebar.height()+75)+'px');
-    });
+    };
+    resizeTimeout(setsidebarminheight);
+    waitforselector('.navBlocks_add', '.mgnlEditor.mgnlPlaceholder', setsidebarminheight);
+
 
     // find the first gato-component paragraph whose top is
     // below the sidebar and expand it to full width
