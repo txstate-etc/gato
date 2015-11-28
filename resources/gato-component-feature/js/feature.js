6c884edfd9cdb08d39f2fbe5713e0f1167fcb71f
Charles Jones <cj32@txstate.edu>
2015-11-28 01:25:51 -0600

    Don't hardcode feature slide autorotate speed in JCR. Also, change values to Michael's specification. #7080

--- resources/gato-component-feature/js/feature.js
+++ resources/gato-component-feature/js/feature.js
@@ -1,11 +1,38 @@
 jQuery(document).ready(function ($) {
+  var NONE = 0, SLOW = 10000, MEDIUM = 7500, FAST = 5000;
+
   $('.gato-slider').each(function() {
     var speed = $(this).attr('data-feature-timer');
+    switch(speed) {
+      case 'none':
+        speed = NONE;
+        break;
+      case 'slow':
+        speed = SLOW;
+        break;
+      case 'medium':
+        speed = MEDIUM;
+        break;
+      case 'fast':
+        speed = FAST;
+        break;
+      default:
+        speed = parseInt(speed, 10);
+        if (isNaN(speed)) {
+          speed = 0;
+        } else {
+          speed *= 1000;
+          if (speed > SLOW) {
+            speed = SLOW;
+          }
+        }
+        break;
+    }
     $(this).find('.slides').slick({
       dots: true,
       adaptiveHeight: true,
       autoplay: !isEditMode && (speed > 0),
-      autoplaySpeed: speed * 1000,
+      autoplaySpeed: speed,
     });
   });
 });
--- resources/gato-component-feature/templates/components/feature.ftl
+++ resources/gato-component-feature/templates/components/feature.ftl
@@ -1,3 +1,3 @@
-<div class="gato-slider" data-feature-timer="${content.defaultTimer!0}" data-feature-carousel="${(content.carousel!false)?string('true','false')}">
+<div class="gato-slider" data-feature-timer="${content.defaultTimer!none}" data-feature-carousel="${(content.carousel!false)?string('true','false')}">
   [@cms.area name="slides" contextAttributes={"colorClass":content.color!} /]
 </div>
