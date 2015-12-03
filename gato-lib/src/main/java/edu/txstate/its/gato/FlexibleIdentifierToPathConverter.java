dad2e52dd483670c4fe1a898839d9521c242a6c1
wickning1 <nwing@txstate.edu>
2015-12-02 19:03:03 -0600

    add identifier conversion to all our linkfielddefinition fields so that we are storing uuids instead of path and users can rename/reorganize pages

---
+++ gato-lib/src/main/java/edu/txstate/its/gato/FlexibleIdentifierToPathConverter.java
@@ -0,0 +1,28 @@
+package edu.txstate.its.gato;
+
+import info.magnolia.ui.form.field.converter.BaseIdentifierToPathConverter;
+import java.util.Locale;
+import java.util.regex.Pattern;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Like Magnolia's BaseIdentifierToPathConverter, but does not return null
+ * when the path doesn't match something, just leaves it alone.
+ * This way users can use the same field to enter internal and external links
+ */
+public class FlexibleIdentifierToPathConverter extends BaseIdentifierToPathConverter {
+  protected final Pattern UUID_PATTERN = Pattern.compile("[a-f0-9]{8}-[a-f0-9]{4}-[1-5][a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}");
+  @Override
+  public String convertToModel(String path, Class<? extends String> targetType, Locale locale) throws ConversionException {
+    if (StringUtils.isBlank(path) || UUID_PATTERN.matcher(path).matches())
+      return super.convertToModel(path, targetType, locale);
+    return path;
+  }
+
+  @Override
+  public String convertToPresentation(String uuid, Class<? extends String> targetType, Locale locale) throws ConversionException {
+    if (StringUtils.isBlank(uuid)) return StringUtils.EMPTY;
+    if (!UUID_PATTERN.matcher(uuid).matches()) return uuid;
+    return super.convertToPresentation(uuid, targetType, locale);
+  }
+}
