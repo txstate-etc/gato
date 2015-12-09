00d771a160474c30f70dc7f4da8d6115db023b28
Charles Jones <cj32@txstate.edu>
2015-12-09 12:13:32 -0600

    Added DragDrop sorting to Feature, Events, Global Links, and Legal Links content apps. #7212

---
+++ gato-lib/src/main/java/edu/txstate/its/gato/GatoDropConstraint.java
@@ -0,0 +1,53 @@
+package edu.txstate.its.gato; 
+
+import info.magnolia.jcr.util.NodeTypes;
+import info.magnolia.ui.workbench.tree.drop.BaseDropConstraint;
+import info.magnolia.ui.vaadin.integration.jcr.JcrNodeAdapter;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.vaadin.data.Item;
+
+public class GatoDropConstraint extends BaseDropConstraint {
+  private static final Logger log = LoggerFactory.getLogger(GatoDropConstraint.class);
+
+  private static final String folderTyoe = NodeTypes.Content.NAME;
+  private static final String nodeType = NodeTypes.Component.NAME;
+
+  public GatoDropConstraint() {
+    super(nodeType);
+  }
+
+  @Override
+  public boolean allowedAsChild(Item sourceItem, Item targetItem) {
+    if (!super.allowedAsChild(sourceItem, targetItem)) {
+      return false;
+    }
+
+    try {
+      JcrNodeAdapter source = (JcrNodeAdapter) sourceItem;
+      JcrNodeAdapter target = (JcrNodeAdapter) targetItem;
+      String sourceNodeType = source.applyChanges().getPrimaryNodeType().getName();
+      String targetNodeType = target.applyChanges().getPrimaryNodeType().getName();
+
+      // If source is a folder and target of nodeType, return false
+      if (folderTyoe.equals(sourceNodeType) && nodeType.equals(targetNodeType)) {
+          log.debug("Could not move a Folder under a node type '{}'", targetNodeType);
+          return false;
+      }
+      // If source is a folder and target is a folder, return false
+      if (folderTyoe.equals(sourceNodeType) && folderTyoe.equals(targetNodeType)) {
+          log.debug("Could not move a Folder under a Folder");
+          return false;
+      }
+
+    } catch (RepositoryException e) {
+      log.warn("Could not check if child is allowed. ", e);
+    }
+    return true;
+  }
+}
