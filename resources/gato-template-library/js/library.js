fa6ef5c2432bd178e5900b407cd5b414ab590c61
Anne Faber <anne.faber@txstate.edu>
2015-12-10 10:51:51 -0600

    add fixed menu behavior to library home page

--- resources/gato-template-library/js/library.js
+++ resources/gato-template-library/js/library.js
@@ -2,4 +2,7 @@ jQuery(document).ready(function ($) {
   // this line is duplicated in txstate2015's common.js
   // probably need a refactoring since the templates share a header but nothing else
   $('.more-tools > a').hovermenu('.super-list-sub');
+
+    // Fixed desktop navigation
+    $('.top_nav').scrollToFixed();
 });
