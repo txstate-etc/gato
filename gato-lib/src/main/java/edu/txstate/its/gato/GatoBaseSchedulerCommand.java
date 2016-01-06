e4a73e472bab99f6d0e9ea6f999655764d5b60b8
wickning1 <nwing@txstate.edu>
2016-01-06 17:39:07 -0600

    get rid of deprecated SystemProperty class, replace with MagnoliaConfigurationProperties

--- gato-lib/src/main/java/edu/txstate/its/gato/GatoBaseSchedulerCommand.java
+++ gato-lib/src/main/java/edu/txstate/its/gato/GatoBaseSchedulerCommand.java
@@ -1,12 +1,20 @@
 package edu.txstate.its.gato;
 
-import info.magnolia.cms.core.SystemProperty;
+import info.magnolia.init.MagnoliaConfigurationProperties;
 import info.magnolia.commands.impl.BaseRepositoryCommand;
+import info.magnolia.objectfactory.Components;
 
 /**
  * @author nwing
  */
 public abstract class GatoBaseSchedulerCommand extends BaseRepositoryCommand {
+  protected final MagnoliaConfigurationProperties mcp;
+
+  public GatoBaseSchedulerCommand() {
+    super();
+    this.mcp = Components.getSingleton(MagnoliaConfigurationProperties.class);
+  }
+
   /**
    * This method returns true unless there is a property in magnolia.properties that
    * says scheduler tasks should be skipped on this installation.
@@ -15,7 +23,7 @@ public abstract class GatoBaseSchedulerCommand extends BaseRepositoryCommand {
    * scheduled tasks run only by one application server.
    */
   public boolean shouldExecute() {
-    if ("true".equals(SystemProperty.getProperty("gato.skipscheduler"))) return false;
+    if ("true".equals(mcp.getProperty("gato.skipscheduler"))) return false;
     return true;
   }
 
