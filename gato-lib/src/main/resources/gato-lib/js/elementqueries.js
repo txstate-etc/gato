7222fb0d57e2753e9fac5edea6c8f8a0a720a99a
wickning1 <nwing@txstate.edu>
2016-01-06 18:55:21 -0600

    refactor elementqueries a bit to allow extra refreshes to be called from the outside when something changes that does not trigger window.resize

--- gato-lib/src/main/resources/gato-lib/js/elementqueries.js
+++ gato-lib/src/main/resources/gato-lib/js/elementqueries.js
@@ -1,37 +1,48 @@
-jQuery(document).ready(function ($) {
-  var watchlist = [];
-  var busy = false;
+(function ($) {
+  window.elementqueries = function () {
+    var eq = window.elementqueries;
+    var watchlist = [];
+    var busy = false;
 
-  var processwidths = function () {
-    for (var i = 0; i < watchlist.length; i++) {
-      var widths = [];
-      var attrs = [];
-      for (var j = 0; j < watchlist[i].length; j++) {
-        widths.push(watchlist[i][j].offsetWidth);
-        attrs.push(watchlist[i][j].getAttribute('data-eq'));
+    eq.processwidths = function () {
+      for (var i = 0; i < watchlist.length; i++) {
+        var widths = [];
+        var attrs = [];
+        for (var j = 0; j < watchlist[i].length; j++) {
+          widths.push(watchlist[i][j].offsetWidth);
+          attrs.push(watchlist[i][j].getAttribute('data-eq'));
+        }
+        for (var j = 0; j < watchlist[i].length; j++) {
+          var w = widths[j];
+          var attrstr = '';
+          for (var k = 1200; k > w; k -= 100) attrstr += k+'px ';
+          if (attrstr != attrs[j]) watchlist[i][j].setAttribute('data-eq', attrstr);
+        }
       }
-      for (var j = 0; j < watchlist[i].length; j++) {
-        var w = widths[j];
-        var attrstr = '';
-        for (var k = 1200; k > w; k -= 100) attrstr += k+'px ';
-        if (attrstr != attrs[j]) watchlist[i][j].setAttribute('data-eq', attrstr);
+      busy = false;
+    };
+
+    eq.observetargets = function () {
+      watchlist = [];
+      $('.eq-parent').each(function () {
+        var eqparent = $(this);
+        var depth = eqparent.parents('.eq-parent').size();
+        if (typeof(watchlist[depth]) == 'undefined') watchlist[depth] = [eqparent.get(0)];
+        else watchlist[depth].push(eqparent.get(0));
+      });
+    };
+
+    eq.refresh = function () {
+      if (!busy && watchlist.length > 0) {
+        busy = true;
+        animationframe(eq.processwidths);
       }
     }
-    busy = false;
+
+    eq.observetargets();
+    eq.refresh();
+    $(window).resize(eq.refresh);
   };
+  $(document).ready(elementqueries);
+})(jQuery);
 
-  $('.eq-parent').each(function () {
-    var eqparent = $(this);
-    var depth = eqparent.parents('.eq-parent').size();
-    if (typeof(watchlist[depth]) == 'undefined') watchlist[depth] = [eqparent.get(0)];
-    else watchlist[depth].push(eqparent.get(0));
-  });
-
-  $(window).resize(function () {
-    if (!busy) {
-      busy = true;
-      animationframe(processwidths);
-    }
-  });
-  processwidths();
-});
