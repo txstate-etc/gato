316b6bf347fe4087966b6d0900c0ef18236da832
wickning1 <nwing@txstate.edu>
2016-03-07 18:34:27 -0600

    converted versionchangedpagescommand to JCR API instead of old Content API, resolves problem with trying to create a version of a deleted page, aside from the general good practice of it all

--- gato-lib/src/main/java/edu/txstate/its/gato/VersionChangedPagesCommand.java
+++ gato-lib/src/main/java/edu/txstate/its/gato/VersionChangedPagesCommand.java
@@ -4,82 +4,75 @@
  */
 package edu.txstate.its.gato;
 
-import info.magnolia.cms.core.Content;
-import info.magnolia.cms.core.HierarchyManager;
-import info.magnolia.cms.core.ItemType;
-import info.magnolia.cms.core.version.ContentVersion;
+import info.magnolia.cms.core.version.VersionManager;
 import info.magnolia.context.Context;
-import info.magnolia.context.MgnlContext;
-// import info.magnolia.module.admininterface.commands.BaseRepositoryCommand;
-import java.util.Iterator;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import info.magnolia.jcr.util.NodeTypes;
+import info.magnolia.jcr.util.NodeUtil;
+
+import java.util.Calendar;
+
+import javax.jcr.Node;
+import javax.jcr.Session;
+import javax.jcr.version.Version;
+import javax.jcr.LoginException;
 import javax.jcr.RepositoryException;
-import javax.jcr.UnsupportedRepositoryOperationException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author seanmctex
  */
 public class VersionChangedPagesCommand extends GatoBaseSchedulerCommand {
+  private static final Logger log = LoggerFactory.getLogger(VersionChangedPagesCommand.class);
+  private VersionManager vm;
 
-    public boolean doExecute(Context context) {
-        log.info("VersionChangedPagesCommand called. Getting Hierarchy Manager...");
-
-        HierarchyManager hm = MgnlContext.getHierarchyManager(this.getRepository());
-        if (hm == null) {
-            log.error("Couldn't get Hiererchy Manager for repository: " + this.getRepository());
-            return false;
-        }
-
-        log.debug("Getting root node...");
-        Content rootNode;
-        try {
-            rootNode = hm.getContent(this.getPath());
-        } catch (RepositoryException ex) {
-            log.error("Couldn't get node: " + this.getPath());
-            return false;
-        }
-
-
-        log.debug("Versioning nodes");
-        versionChangedPages(rootNode);
-
-        log.info("VersionChangedPagesCommand completed.");
-
-        return true;
+  public boolean doExecute(Context context) throws LoginException, RepositoryException {
+    log.info("VersionChangedPagesCommand called. Getting JCR Session...");
+    Session s = context.getJCRSession(this.getRepository());
+    if (s == null) {
+      log.error("Couldn't get JCR Session for repository: " + this.getRepository());
+      return false;
     }
 
-    private void versionChangedPages(Content c) {
-        Iterator childNodesIterator = c.getChildren(ItemType.CONTENT).iterator();
-        while (childNodesIterator.hasNext()) {
-            Content page = (Content) childNodesIterator.next();
-            log.debug("Visiting page: " + page.getHandle());
-            try {
-                ContentVersion baseVersion = page.getBaseVersion();
-                log.debug("Page date: " + page.getMetaData().getModificationDate().toString());
-                log.debug("Base version date: " + baseVersion.getMetaData().getModificationDate().toString());
-                if (page.getMetaData().getModificationDate().after(baseVersion.getMetaData().getModificationDate())) {
-                    log.debug("I NEED BACKUP!");
-                    versionPage(page);
-                }
-            } catch (UnsupportedRepositoryOperationException ex) {
-                Logger.getLogger(VersionChangedPagesCommand.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (RepositoryException ex) {
-                log.debug("Page has no stored version. Creating one now.");
-                versionPage(page);
-            }
-            versionChangedPages(page);
-        }
+    log.info("Getting VersionManager...");
+    vm = VersionManager.getInstance();
 
+    log.debug("Getting root node...");
+    Node rootNode;
+    try {
+      rootNode = s.getNode(this.getPath());
+    } catch (RepositoryException ex) {
+      log.error("Couldn't get node: " + this.getPath());
+      return false;
     }
 
-    private void versionPage(Content page) {
-        try {
-            log.info("Versioning page: " + page.getHandle() );
-            page.addVersion();
-        } catch (Exception ex1) {
-            Logger.getLogger(VersionChangedPagesCommand.class.getName()).log(Level.SEVERE, null, ex1);
+    log.debug("Versioning nodes");
+    versionChangedPages(rootNode);
+
+    log.info("VersionChangedPagesCommand completed.");
+    return true;
+  }
+
+  private void versionChangedPages(Node page) throws RepositoryException {
+    for (Node subpage : NodeUtil.getNodes(page, NodeTypes.Page.NAME)) {
+      log.debug("Visiting page: " + subpage.getPath());
+
+      try {
+        Version baseVersion = vm.getBaseVersion(subpage);
+        Calendar versiondate = baseVersion.getCreated();
+        Calendar lastmod = NodeTypes.LastModified.getLastModified(subpage);
+        log.debug("Page date: " + lastmod.toString());
+        log.debug("Base version date: " + versiondate.toString());
+        if (lastmod.after(versiondate)) {
+          log.debug("I NEED BACKUP!");
+          Version newVersion = vm.addVersion(subpage);
         }
+      } catch (Exception e) {
+        log.error("Unable to add a version for this page.", e);
+      }
+      versionChangedPages(subpage);
     }
+  }
 }
