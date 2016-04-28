f77eb78aa55b83369523ef7759f31fc5123309df
Anne Faber <anne.faber@txstate.edu>
2016-04-28 12:16:00 -0500

    Comment out JS that was causing error.  Minor style tweaks for text field dialog #7499

--- gato-area-mail/src/main/resources/gato-area-mail/js/formedit.js
+++ gato-area-mail/src/main/resources/gato-area-mail/js/formedit.js
@@ -1,3 +1,11 @@
+/* The senderInfo radio buttons are not there anymore.  I'm not deleting this file
+*  because I think we will need it for something else.  In the meantime, this code
+*  is commented out because the missing senderInfo field was causing an error
+*/
+
+
+
+
 // The vaadin combobox isn't implemented using a standard select.
 // Instead, it creates a table element containing the choices when
 // the dropdown is clicked. The email address data type needs to
@@ -8,36 +16,37 @@
 function initFormEdit(def, node, el) {
   $(el).closest('.v-form-field-section').hide();
 
-  // Hate to use setTimeout here, but still haven't figured out a reliable
-  // way to know if the other vaadin fields are loaded before the js include.
-  setTimeout(function() {
-    var senderInfoRadio = $('.senderInfo').find('input')[2];
-    var dataTypeInput = $('.dataType').find('input');
-    var overlayContainer = $('.v-overlay-container')[0];
-    var restoreRadioFocus = false;
 
-    function setDataTypeToEmail() {
-      $('.dataType').find('.v-filterselect-button')[0].click()
-      observer.observe(overlayContainer, {childList: true});
-    }
+  // // Hate to use setTimeout here, but still haven't figured out a reliable
+  // // way to know if the other vaadin fields are loaded before the js include.
+  // setTimeout(function() {
+  //   var senderInfoRadio = $('.senderInfo').find('input')[2];
+  //   var dataTypeInput = $('.dataType').find('input');
+  //   var overlayContainer = $('.v-overlay-container')[0];
+  //   var restoreRadioFocus = false;
 
-    var observer = new MutationObserver(function(mutations) {
-      if ($('.v-filterselect-suggestmenu').find('tr').length) {
-        $('.v-filterselect-suggestmenu').find('tr').find('span')[5].click();
-        if (restoreRadioFocus) senderInfoRadio.focus();
-        observer.disconnect();
-      }    
-    });
+  //   function setDataTypeToEmail() {
+  //     $('.dataType').find('.v-filterselect-button')[0].click()
+  //     observer.observe(overlayContainer, {childList: true});
+  //   }
 
-    $(senderInfoRadio).change(function() {
-      if (this.checked && dataTypeInput.value != "E-Mail Address") {
-        restoreRadioFocus = true;
-        setDataTypeToEmail();
-      }
-    });
+  //   var observer = new MutationObserver(function(mutations) {
+  //     if ($('.v-filterselect-suggestmenu').find('tr').length) {
+  //       $('.v-filterselect-suggestmenu').find('tr').find('span')[5].click();
+  //       if (restoreRadioFocus) senderInfoRadio.focus();
+  //       observer.disconnect();
+  //     }    
+  //   });
 
-    if (senderInfoRadio.checked && dataTypeInput.value != "E-Mail Address") {
-      setDataTypeToEmail();  
-    }
-  }, 500);
+  //   $(senderInfoRadio).change(function() {
+  //     if (this.checked && dataTypeInput.value != "E-Mail Address") {
+  //       restoreRadioFocus = true;
+  //       setDataTypeToEmail();
+  //     }
+  //   });
+
+  //   if (senderInfoRadio.checked && dataTypeInput.value != "E-Mail Address") {
+  //     setDataTypeToEmail();  
+  //   }
+  // }, 500);
 }
