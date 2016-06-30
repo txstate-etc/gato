7ea5a7cdd4849ca9d861f24157874bf9d84c40a0
wickning1 <nwing@txstate.edu>
2016-06-30 18:53:44 -0500

    added analytics to gato-edit #7523

--- gato-lib/src/main/resources/VAADIN/js/onload.js
+++ gato-lib/src/main/resources/VAADIN/js/onload.js
@@ -1,1 +1,11 @@
 // This script will be loaded when adminCentral is loaded
+
+if (window.location.href.indexOf("gato-edit.its") > -1) {
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-7250696-3', 'auto');
+  ga('send', 'pageview');
+}
