669e13cf9505a0736a8d1886cc308945e580eb9a
Nick Wing <nwing@txstate.edu>
2016-08-09 19:09:49 -0500

    added the google search appliance code chris wrote to be used for site search

--- resources/gato-template-txstate2015/templates/pages/standard-template.ftl
+++ resources/gato-template-txstate2015/templates/pages/standard-template.ftl
@@ -5,6 +5,7 @@
 	<head>
 		<link rel="stylesheet" type="text/css" href="${gf.resourcePath()}/gato-template-txstate2015/css/txstate2015.compiled.css"/>
     [#assign jsscripts = [
+      'gato-template/js/search-gsa.js',
       'gato-template-txstate2015/js/jquery-scrolltofixed-min.js',
       'gato-template-txstate2015/js/respond.min.js',
       'gato-template-txstate2015/js/slideout.js',
---
+++ resources/gato-template/js/search-gsa.js
@@ -0,0 +1,63 @@
+(function($) {
+window.Search = function(opts) {
+  opts = opts || {};
+  if (!(this instanceof Search)) {
+    return new Search(opts);
+  }
+  this.opts = {
+    num: opts.num || 5,
+    start: opts.start || 0,
+    url: opts.url || 'http://search.txstate.edu/search',
+    site: opts.site || 'txstate.edu'
+  }
+}
+
+Search.prototype.doSearch = function(query) {
+  var self = this;
+  self.query = query;
+  var params = {
+    url: self.opts.url,
+    site: 'txstate_no_users',
+    client: 'txstate',
+    output: 'xml_no_dtd',
+    sitesearch: self.opts.site,
+    start: self.opts.start,
+    num: self.opts.num,
+    q: self.query
+  };
+
+  var dfd = $.Deferred();
+  $.ajax({
+    url: params.url,
+    data: params,
+    success: function(data, textStatus, jqXHR) {
+      console.log(data);
+      var result = [];
+      result.total = parseInt($(data).find('M').text());
+      result.type = "web";
+      result.results = $(data).find('R').map(function() {
+        var url = $(this).find('U').text();
+        var url_display = url.replace(/^\w+:\/\//, '');
+        if (url_display.length > 40) url_display = url_display.substr(0,40)+"...";
+        var title = $(this).find('T').text() || url;
+        var summary = $(this).find('S').text() || 'No summary available.';
+        return {
+          title: title,
+          summary_html: summary,
+          url: url,
+          url_display: url_display
+        }
+      }).get();
+      dfd.resolve(result);
+    },
+    error: function(jqXHR, textStatus) {
+      dfd.reject({
+        pages: [],
+        error: textStatus
+      });
+    }
+  });
+
+  return dfd.promise();
+}
+})(jQuery);
