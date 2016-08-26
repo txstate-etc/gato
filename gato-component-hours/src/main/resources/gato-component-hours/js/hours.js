48d4bd839a34341fedcbaf89b863ffc3967d67c4
Nick Wing <nwing@txstate.edu>
2016-08-26 17:31:26 -0500

    make alkek hours modal more accessible

--- gato-component-hours/src/main/resources/gato-component-hours/js/hours.js
+++ gato-component-hours/src/main/resources/gato-component-hours/js/hours.js
@@ -1,13 +1,34 @@
 jQuery(document).ready(function ($) {
+  var show_modal = function () {
+    $('.hours-modal').show();
+    $('body > *').each( function () {
+      if (!$(this).is('.hours-modal')) {
+        this.modal_disabled = true;
+        this.save_aria_disable = $(this).attr('aria-disabled');
+        $(this).attr('aria-disabled', 'true');
+      }
+    });
+  };
+
+  var hide_modal = function () {
+    $('.hours-modal').hide();
+    // undo what we did to aria-disabled on show()
+    $('body > *').each( function () {
+      if (!$(this).is('.hours-modal')) {
+        if (this.modal_disabled) $(this).attr('aria-disabled', this.save_aria_disable);
+        this.modal_disabled = false;
+      }
+    });
+  };
+
   $('.hours-full').click(function(e) {
     if ($('#hours-container').size() == 0) {
       $('body').append('<div class="hours-modal"><div id="hours-container">');
       $('.hours-modal').click(function (e) {
-        if (!$(e.target).is('.fc-button, .fc-button *'))
-          $('.hours-modal').hide();
+        if (!$(e.target).is('.fc-button, .fc-button *')) hide_modal();
       });
     } else {
-      $('.hours-modal').show();
+      show_modal();
     }
     var lnk = $(this);
     $('#hours-container').fullCalendar({
