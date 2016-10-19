2747d556afdfb1fb19e772035852970d6ba30cc1
Nick Wing <nwing@txstate.edu>
2016-10-19 13:30:10 -0500

    update to new CAS service at login.its...

--- gato-internal/src/main/java/edu/txstate/its/gato/setup/GatoInternalVersionHandler.java
+++ gato-internal/src/main/java/edu/txstate/its/gato/setup/GatoInternalVersionHandler.java
@@ -98,6 +98,7 @@ public class GatoInternalVersionHandler extends DefaultModuleVersionHandler {
 
     register(DeltaBuilder.update("1.0.5", "")
       .addTask(new BootstrapSingleModuleResource("config.modules.pages.dialogs.createPage.xml"))
+      .addTask(new BootstrapSingleModuleResource("config.modules.cas.config.xml"))
     );
   }
 
