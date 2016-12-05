947cb5258d801ba524d5a19ca054f3d8f7254bea
Nick Wing <nwing@txstate.edu>
2016-12-05 14:35:22 -0600

    add a rest script for fixing pdf mime types in Assets, was causing the download to hang

---
+++ scripts/update_text_pdf_assets.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use strict;
+use File::Basename qw(dirname);
+
+require(dirname(__FILE__).'/gato_restful_lib.pl');
+our $default_repo = "dam";
+
+$|=1;
+exit(main());
+
+sub main {
+	my $json = query("select * from [mgnl:resource] where [jcr:mimeType]='text/pdf'");
+
+	for my $node ( @{$json} ) {
+		print "evaluating resource at ".$$node{path}."...";
+		if (setproperty($node, 'jcr:mimeType', 'application/pdf')) {
+			print "SUCCESS!\n";
+		}
+	}
+	print "finished!\n";
+}
