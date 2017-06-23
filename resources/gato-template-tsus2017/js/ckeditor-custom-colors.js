18e9e2beabab7ef9e3e36ed6e1af95a71005723a
Anne Faber <anne.faber@txstate.edu>
2017-06-23 14:52:43 -0500

    Add colors for rich editor tables

---
+++ resources/gato-template-tsus2017/js/ckeditor-custom-colors.js
@@ -0,0 +1,22 @@
+window.cktemplatevars = {
+    definition_colors: "37333d,8e857b,8f2024,1F355E,89A18F,8499a1,2755AA",
+    header_color_options: [ ['None', 'header-color-none'],
+                         [ 'Default (Midnight)', 'header-color-midnight' ],
+                         [ 'Taupe', 'header-color-taupe' ],
+                         [ 'Red','header-color-red' ],
+                         [ 'Navy', 'header-color-navy'],
+                         [ 'Sea Green', 'header-color-seagreen'],
+                         [ 'Ocean', 'header-color-ocean' ],
+                         [ 'Blue', 'header-color-tsusblue']
+                         ],
+    default_header_color_class: 'header-color-midnight',
+    template_colors: [ ['Midnight', '#37333D' ],
+                        ['Taupe', '#8E857B'],
+                        ['Red', '#8f2024'],
+                        ['Navy', '#1F355E'],
+                        ['Sea Green', '#89A18F'],
+                        ['Ocean', '#8499a1'],
+                        ['Blue', '#2755AA']
+                     ],
+    template_color_label: "TSUS Colors"
+}
