e31ffc3456aef1a46c667525f2e8910fef68d34d
Nick Wing <nwing@txstate.edu>
2017-07-25 20:37:32 -0500

    fixed tsuslogos to automagically find the ideal column count at any screen width

--- resources/gato-template-tsus2017/js/tsus.js
+++ resources/gato-template-tsus2017/js/tsus.js
@@ -17,66 +17,36 @@ jQuery(document).ready(function($) {
     });
 
 
-    function setWidth($logoVar,$column){
-      var $logoWidth;//in order to force column count
-      $logoWidth=100.0/$column;//100% divided by column count.
-      $logoVar.css("width",$logoWidth+"%");
-      console.log('columns:'+$column);// for testing
-      console.log('width of columns:'+$logoWidth);// for testing
-    }
-    function setColumn($divider,$tCount){
-      var $column;
-      var $optimize = $tCount/$divider;// what we will flip by if needed
-      if(($optimize)>4)//max of 4 columns if more than 4 divide again
-      {
-        $column=($tCount/$divider)/$divider;
-        if($column<2)
-        {
-          console.log("optimizing grid structure.....");
-          $column=($tCount/$optimize);
+    function evaluate_tsus_logos() {
+      var container = $('.gato-flex-container');
+      var logos = container.find('li');
+      var logocount = logos.size();
+      var minwidth = 100;
+      var maxwidth = 300;
+      var absolutemaxcols = Math.min(logocount, 12);
+      var containerwidth = container.width();
+      var maxcols = Math.min(Math.floor(containerwidth / minwidth), absolutemaxcols);
+      var mincols = Math.max(Math.ceil(containerwidth / maxwidth), 1);
+      var currentbest = maxcols;
+      for (var i = mincols; i <= maxcols; i++) {
+        var proposedremainder = (i - (logocount % i)) % i;
+        if (proposedremainder < 2) currentbest = i;
+      }
+      container.removeClass(function (idx, classname) {
+        return (classname.match (/flex-columns-\S+/g) || []).join(' ');
+      }).addClass('flex-columns-'+currentbest);
 
-        }
-      }
-      else{
-          $column=$tCount/$divider;
-      }
-      console.log("2nd func is working")
-      return $column;
-    }
-    function setGrid($logoVar,$tCount){
-      var $gridSet=false;
-      var $column;
-      if($tCount%2==0){
-          $column = setColumn(2,$tCount);
-          setWidth($logoVar,$column);
-          $gridSet=true;
-          return $gridSet;
-      }
-      else if ($tCount%3==0) {
-        $column = setColumn(3,$tCount);
-        setWidth($logoVar,$column);
-        $gridSet=true;
-        return $gridSet;
-      }
-    }
-
-    resizeTimeout(function(){
-        var $logoVar=$('.tsus-institution-logos li');
-        var $tCount=$logoVar.length;
-        var $gridSet = false;
-      	if ($(window).width() <= 768){
-            $gridSet=setGrid($logoVar,$tCount);
-          if(!$gridSet)// meaning its prime
-            {
-              console.log('prime detected!....resolving');
-              $tCount=$tCount + 1;// add 1 then call again.
-              $gridSet=setGrid($logoVar,$tCount);
-            }
-      	}
-        else{
-            $logoVar.css("width","auto");
-        }
+      logos.removeClass(function (idx, classname) {
+        return (classname.match (/flex-remainder-\S+/g) || []).join(' ');
       });
+      var currentremainder = logocount % currentbest;
+      if (currentremainder > 0) {
+        logos.eq(-currentremainder).addClass('flex-remainder-left');
+        logos.eq(-1).addClass('flex-remainder-right');
+      }
+    }
+
+    resizeTimeout(evaluate_tsus_logos);
 });
 
 jQuery(window).on('load', function($) {
