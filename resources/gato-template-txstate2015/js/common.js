30ba7734946d38d52a2e6544202f4816711d4e70
Anne Faber <anne.faber@txstate.edu>
2017-08-03 15:58:54 -0500

    Remove hover background color from imagecard links inside masonry sections

--- resources/gato-template-txstate2015/js/common.js
+++ resources/gato-template-txstate2015/js/common.js
@@ -150,7 +150,7 @@ jQuery(document).ready(function($) {
   $('.more-tools > a').hovermenu('.super-list-sub');
 
   //remove hover effect from rich editor image and font awesome links
-  $('.column_paragraph a, .footer_content a, .txst-form-body a').each(function(){
+  $('.column_paragraph a, .footer_content a, .txst-form-body a, .masonry-section .gato-card a').each(function(){
     if($(this).find('img, .fa').length > 0){
         $(this).addClass('link-with-children');
     }
