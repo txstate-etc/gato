707a2062a980ffb8a00e216df41ddb34379b3de7
spaceage1217 <dimeji.faluyi@gmail.com>
2017-08-09 15:39:15 -0500

    fixed auto thumbnail for youtube and vimeo through javascript wizardry

--- resources/gato-template/js/masonry.js
+++ resources/gato-template/js/masonry.js
@@ -8,11 +8,24 @@ jQuery(window).on('load', function($) {
     percentPosition: true,
   });
   setTimeout(function(){$grid.masonry('layout');},1000);
-  //for adding youtube or Vimeo thumbnails if no custom thumbnail is added
-  $('.gato-card-streaming-wrap a').each(function(index) {
-    if(!$('.gato-card-streaming .img-box img').eq(index).hasClass('userThumbnail')){
-      var linkArray=this.getAttribute('href').split('/');
-      if(linkArray[2]==='vimeo.com'){
+  /*
+    For adding youtube or Vimeo thumbnails if no custom thumbnail is added
+    className will be set to .img-box first child className.
+    If that className is userThumbnail then that means the user has added a thumbnail
+    which means theres no need to fetch a thumbnail.
+    Only works for vimeo and youtube videos for now.
+
+  */
+  $('.gato-card-streaming-wrap .img-box').each(function(index) {
+    let className = $(this).siblings().context.firstElementChild.className;
+    let userThumbnail=false;
+    (className==='userThumbnail')? userThumbnail = true:userThumbnail=false;
+    
+    if(!userThumbnail){
+      var linkArray= $('.gato-card-streaming-wrap a').eq(index)[0].href.split('/');
+      let vimeo = linkArray[2]==='vimeo.com';
+      let youtube =linkArray[2]==='www.youtube.com';
+      if(vimeo){
         var vimeoID=linkArray[linkArray.length-1];//grab the last part of the link which is the id for vimeo.
         $.getJSON('http://vimeo.com/api/oembed.json?url=http%3A//vimeo.com/'+vimeoID, function(video) {
             console.log(video.thumbnail_url);
@@ -21,7 +34,7 @@ jQuery(window).on('load', function($) {
               $grid.masonry('layout');
         });
       }
-      else if (linkArray[2]==='www.youtube.com') {
+      else if (youtube) {
           //if its a youtube video further split the end of the link which -
           //-follows the pattern watch?v='some-id-num and grab the last value in the array'
           var youtubeLink=linkArray[linkArray.length-1].split('=');
@@ -33,4 +46,5 @@ jQuery(window).on('load', function($) {
       }
     }
   });
+
 });
