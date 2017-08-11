9933a21971d446f630f371ef6ccdedebd68937ed
Nick Wing <nwing@txstate.edu>
2017-08-11 17:06:14 -0500

    converted faq hierarchy to jquery and velocity instead of prototype and scriptaculous, also greatly improved accessibility by implementing the tree pattern

--- gato-component-faq/src/main/resources/gato-component-faq/js/expand.js
+++ gato-component-faq/src/main/resources/gato-component-faq/js/expand.js
@@ -1,101 +1,54 @@
-Event.observe(window, "load", function() {
-  $$('.txst-faqitem-question a').each( function( item ) {
-    var para = item.up('.txst-contentarea-paragraph');
-    if (!para) para = item.up('.iphone-contentarea-paragraph');
-    if (para) para.setStyle({overflow: 'hidden', margin: '10px 0px'});
-    var answerElement = item.ancestors()[0].next();
-    answerElement.setStyle({display: 'none'});
-    var caretElement = item.down('.caret');
-    Event.observe( item, "click", function( event ) {
-      item.blur();
-      //answerElement.toggle();
-      if(answerElement.visible()){
-        Effect.BlindUp( answerElement, { duration: 0.15 } );
-        if(caretElement){
-          caretElement.removeClassName('fa fa-caret-down');
-          caretElement.addClassName('fa fa-caret-right');
-        }
-      }
-      else{
-        Effect.BlindDown( answerElement, { duration: 0.15 } );
-        if(caretElement){
-          caretElement.removeClassName('fa fa-caret-right');
-          caretElement.addClassName('fa fa-caret-down');
-        }
-      }
-      Event.stop( event );
-    });
-  });
-
-  $$('.txst-faq-group-title a').each(function(item) {
-    var groupElement = item.up().next('.txst-faq-group');
-
-    Event.observe(item, 'click', function(event) {
-      item.blur();
-      //groupElement.toggle();
-      if(groupElement.visible()){
-        Effect.BlindUp(groupElement, { duration: 0.15});
-      }
-      else{
-        Effect.BlindDown(groupElement,{ duration: 0.15});
-      }
-      Event.stop(event);
-    });
-  });
-
-  if ( $('txst-expand-all-faqs') ) {
-    Event.observe( 'txst-expand-all-faqs', "click", function(event) {
-      $$('.txst-faq-group').each(function(item) {
-        if (!item.visible()) Effect.BlindDown(item, {duration: 0.15 });
-      });
-      $$('.txst-faqitem-answer').each( function( item ) {
-        if ( !item.visible() ) Effect.BlindDown( item, { duration: 0.15 } );
-      });
-      $$('.caret').each( function( item ) {
-        item.removeClassName('fa fa-caret-right');
-        item.addClassName('fa fa-caret-down');
-      });
-      Event.stop(event);
-    });
-  }
-
-  if ( $('txst-collapse-all-faqs') ) {
-    Event.observe( 'txst-collapse-all-faqs', "click", function(event) {
-      $$('.txst-faqitem-answer').each( function( item ) {
-        if ( item.visible() ) Effect.BlindUp( item, { duration: 0.15 } );
-      });
-      $$('.caret').each( function( item ) {
-        item.removeClassName('fa fa-caret-down');
-        item.addClassName('fa fa-caret-right');
-      });
-      Event.stop(event);
-    });
-  }
-
-  openFaqAnchor();
-});
-
-// Using jQuery here for compatability with a handler in common.js
-// TODO: convert the rest of this file to jQuery
 jQuery(document).ready(function($) {
-  $('a[href^="#"]:not([href="#"])').click(function(event) {
-    // There's already some code in common.js to handle anchor clicks
-    // Just show the faq item, so the other handler works properly.
-    openFaqAnchor();
-  });
-});
-
-function openFaqAnchor() {
-  var anchorName = location.hash.slice(1);
-  if (anchorName) {
-    var anchor = $$('.txst-faqitem a[name="' + anchorName + '"]')[0];
-    if (anchor) {
-      var faqitem = anchor.up('.txst-faqitem');
-      var caret = faqitem.down('.caret');
-      faqitem.down('.txst-faqitem-answer').show();
-      if (caret) {
-        caret.removeClassName('fa-caret-right').addClassName('fa-caret-down');
-      }
+  var show = function ($lnk, options) {
+    options = options || {};
+    var $li = $lnk.closest('li');
+    var $panel = $lnk.next();
+    $li.addClass('shown');
+    $li.attr('aria-expanded', true);
+    if (options.instant) {
+      $panel.show();
+    } else {
+      $panel.velocity('slideDown', { duration: 150 });
     }
   }
-}
+
+  var hide = function($lnk, options) {
+    options = options || {};
+    var $li = $lnk.closest('li');
+    var $panel = $lnk.next();
+    $li.removeClass('shown');
+    $li.attr('aria-expanded', false);
+    if (options.instant) {
+      $panel.hide();
+    } else {
+      $panel.velocity('slideUp', { duration: 150 });
+    }
+  }
+
+  var toggle = function($lnk) {
+    var $li = $lnk.closest('li');
+    if ($li.hasClass('shown')) hide($lnk);
+    else show($lnk);
+  }
+
+  $('.gato-faqitem-question, .gato-faq-group-title').click(function(e) {
+    e.preventDefault();
+    toggle($(this));
+  });
+
+  $('#gato-expand-all-faqs').click(function(e) {
+    e.preventDefault();
+    show($('.gato-faqitem-question, .gato-faq-group-title'));
+  });
+  $('#gato-collapse-all-faqs').click(function(e) {
+    e.preventDefault();
+    hide($('.gato-faqitem-question, .gato-faq-group-title'));
+  });
+
+  // if the url contains an anchor that lives inside an faq, let's open the FAQ to that item
+  $('.gato-faqitem-answer '+location.hash).each(function (i, ele) {
+    var $lis = $(ele).parents('.gato-faqitem, .gato-faq-group');
+    var $lnks = $lis.find('> .gato-faqitem-question, > .gato-faq-group-title')
+    show($lnks, {instant: true});
+  });
+});
--- gato-component-faq/src/main/resources/gato-component-faq/templates/components/faq-hierarchy.ftl
+++ gato-component-faq/src/main/resources/gato-component-faq/templates/components/faq-hierarchy.ftl
@@ -1,21 +1,21 @@
 [#macro faqItem node depth]
-  <div class="txst-faqitem" style="margin-left:${20*depth}px">
-    <h2 class="txst-faqitem-question">
-      <a href="#">
-        [#if depth > 0]<span class="caret fa fa-caret-right"></span>[/#if]
-        ${node.question}</a>
-    </h2>
-    <div class="txst-faqitem-answer">${node.answer}</div>
-  </div>
+  <li class="gato-faqitem" style="margin-left:${20*depth}px" role="treeitem" aria-expanded="false">
+    <a href="#" class="gato-faqitem-question">
+      [#if depth > 0]<i class="fa fa-caret-right" aria-hidden="true"></i>[/#if]
+      ${node.question}</a>
+    <div class="gato-faqitem-answer" role="group" aria-hidden="true"><div role="treeitem">${node.answer}</div></div>
+  </li>
 [/#macro]
 
 [#macro faqGroup node depth]
-  <h2 class="txst-faq-group-title" style="margin-left:${20*depth}px"><a href="#">${node.title}</a></h2>
-  <div class="txst-faq-group">
-    [#list node.children as child]
-      [@faqNode child depth+1 /]
-    [/#list]
-  </div>
+  <li class="gato-faq-group" role="treeitem" aria-expanded="false">
+    <a href="#" class="gato-faq-group-title">${node.title}</a>
+    <ul class="gato-faq-group-children" role="group" aria-hidden="true">
+      [#list node.children as child]
+        [@faqNode child depth+1 /]
+      [/#list]
+    </ul>
+  </li>
 [/#macro]
 
 [#macro faqNode node depth]
@@ -26,15 +26,15 @@
   [/#if]
 [/#macro]
 
-<div class="faq-hierarchy">
 [#if !ctx.request.getAttribute('faqButtonsAdded')!false]
   <div class="gato-faq-expand-collapse">
-    <a href="#" id="txst-expand-all-faqs">Expand</a> or
-    <a href="#" id="txst-collapse-all-faqs">Collapse</a> all.
+    <a href="#" id="gato-expand-all-faqs">Expand</a> or
+    <a href="#" id="gato-collapse-all-faqs">Collapse</a> all.
   </div>
   ${ctx.request.setAttribute('faqButtonsAdded', true)}
 [/#if]
+<ul class="gato-faq-hierarchy" role="tree">
 [#list model.nodes as node]
   [@faqNode node 0 /]
 [/#list]
-</div>
+</ul>
