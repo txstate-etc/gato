87fc50baa61ce541f73c372fec56134590c88a8c
wickning1 <git@mythgaming.net>
2017-08-12 12:42:39 -0500

    brought accordion code that runs RSS and Calendar content types into closer sync with faq hierarchy

--- gato-component-faq/src/main/resources/gato-component-faq/js/expand.js
+++ gato-component-faq/src/main/resources/gato-component-faq/js/expand.js
@@ -1,50 +1,16 @@
 jQuery(document).ready(function($) {
-  var show = function ($lnk, options) {
-    options = options || {};
-    var $li = $lnk.closest('li');
-    var $panel = $lnk.next();
-    $li.addClass('shown');
-    $li.attr('aria-expanded', true);
-    $panel.attr('aria-hidden', false);
-    if (options.instant) {
-      $panel.show();
-    } else {
-      $panel.velocity('slideDown', { duration: 150 });
-    }
-  }
-
-  var hide = function($lnk, options) {
-    options = options || {};
-    var $li = $lnk.closest('li');
-    var $panel = $lnk.next();
-    $li.removeClass('shown');
-    $li.attr('aria-expanded', false);
-    $panel.attr('aria-hidden', true);
-    if (options.instant) {
-      $panel.hide();
-    } else {
-      $panel.velocity('slideUp', { duration: 150 });
-    }
-  }
-
-  var toggle = function($lnk) {
-    var $li = $lnk.closest('li');
-    if ($li.hasClass('shown')) hide($lnk);
-    else show($lnk);
-  }
-
   $('.gato-faqitem-question, .gato-faq-group-title').click(function(e) {
     e.preventDefault();
-    toggle($(this));
+    window.accordion.toggle($(this));
   });
 
   $('#gato-expand-all-faqs').click(function(e) {
     e.preventDefault();
-    show($('.gato-faqitem-question, .gato-faq-group-title'));
+    window.accordion.show($('.gato-faqitem-question, .gato-faq-group-title'));
   });
   $('#gato-collapse-all-faqs').click(function(e) {
     e.preventDefault();
-    hide($('.gato-faqitem-question, .gato-faq-group-title'));
+    window.accordion.hide($('.gato-faqitem-question, .gato-faq-group-title'));
   });
 
   // if the url contains an anchor that lives inside an faq, let's open the FAQ to that item
@@ -52,7 +18,7 @@ jQuery(document).ready(function($) {
     $('.gato-faqitem-answer '+document.location.hash+', .gato-faqitem-answer [name="'+document.location.hash.slice(1)+'"]').each(function (i, ele) {
       var $lis = $(ele).parents('.gato-faqitem, .gato-faq-group');
       var $lnks = $lis.find('> .gato-faqitem-question, > .gato-faq-group-title')
-      show($lnks, {instant: true});
+      window.accordion.show($lnks, {instant: true});
     });
   }
 });
--- resources/gato-template/js/accordion.js
+++ resources/gato-template/js/accordion.js
@@ -1,30 +1,67 @@
 (function($) {
   var accordion_dur = 150;
+  window.accordion = {
+    show : function ($lnk, options) {
+      options = options || {};
+      var $parent = $lnk.parent();
+      var $panel = $lnk.next();
+      $parent.addClass('shown');
+      $parent.attr('aria-expanded', true);
+      $panel.attr('aria-hidden', false);
+      if (options.instant) {
+        $panel.show();
+      } else {
+        $panel.velocity('slideDown', { duration: accordion_dur });
+      }
+    },
+
+    hide : function($lnk, options) {
+      options = options || {};
+      var $parent = $lnk.parent();
+      var $panel = $lnk.next();
+      $parent.removeClass('shown');
+      $parent.attr('aria-expanded', false);
+      $panel.attr('aria-hidden', true);
+      if (options.instant) {
+        $panel.hide();
+      } else {
+        $panel.velocity('slideUp', { duration: accordion_dur });
+      }
+    },
+
+    toggle : function($lnk) {
+      var $parent = $lnk.parent();
+      if ($parent.hasClass('shown')) window.accordion.hide($lnk);
+      else window.accordion.show($lnk);
+    }
+  }
 
   $(function() {
-    $('.gato-accordion[data-start-collapsed=true] .gato-accordion-content').hide();
-    $('.gato-accordion[data-start-collapsed=true] .gato-accordion-header').removeClass('selected');
-    $('.gato-accordion[data-start-collapsed=false] .gato-accordion-header').addClass('selected');
+    window.accordion.hide($('.gato-accordion[data-start-collapsed=true] .gato-accordion-header'), {instant: true});
+    window.accordion.show($('.gato-accordion[data-start-collapsed=false] .gato-accordion-header'), {instant: true});
 
     $('.gato-accordion-header').on('click', function(e) {
       e.preventDefault();
-      $(this).toggleClass('selected');
-      $(this).next('.gato-accordion-content').slideToggle(accordion_dur);
+      window.accordion.toggle($(this));
     });
 
     $('#gato-accordion-expand-all').on('click', function(e) {
       e.preventDefault();
-      var $accordions = $('.gato-accordion');
-      $accordions.find('.gato-accordion-content').slideDown(accordion_dur);
-      $accordions.find('.gato-accordion-header').addClass('selected');
+      window.accordion.show($('.gato-accordion-header'));
     });
 
     $('#gato-accordion-collapse-all').on('click', function(e) {
       e.preventDefault();
-      var $accordions = $('.gato-accordion');
-      $accordions.find('.gato-accordion-content').slideUp(accordion_dur);
-      $accordions.find('.gato-accordion-header').removeClass('selected');
+      window.accordion.hide($('.gato-accordion-header'));
     });
 
+    // if the url contains an anchor that lives inside an faq, let's open the FAQ to that item
+    if (document.location.hash.match(/^#[a-z][\w\-\:\.]*$/i)) {
+      $('.gato-accordion-content '+document.location.hash+', .gato-accordion-content [name="'+document.location.hash.slice(1)+'"]').each(function (i, ele) {
+        var $lis = $(ele).parents('.gato-accordion');
+        var $lnks = $lis.find('> .gato-accordion-header');
+        window.accordion.show($lnks, {instant: true});
+      });
+    }
   });
 })(jQuery);
