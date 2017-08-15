8cd5f568d91f97a15204db10aecc8c7612ad3d46
Jeff Snider <jsnider@txstate.edu>
2017-08-15 18:24:37 -0500

    Close to working filters.

--- resources/gato-component-cards/js/gridlayout.js
+++ resources/gato-component-cards/js/gridlayout.js
@@ -5,27 +5,57 @@ jQuery(function() {
 
 
 	var filterable_grid = function(element){
+		var filterable_grid = this;
 		var gridid = element.id;
-
-		var activetab = getHashParameters()[gridid+"_f"];
 		var filters = [];
 
-		$(element).find('ul.grid-filter>li>a').each(function(i,filtertab){
-			filters.push({
-				linkelement : filtertab,
-				name : $(filtertab).text(),
-				selected : $(filtertab).text() == activetab ? true : false
+		var init_filters = function(){
+			// Get the current selected filter from the hash parameter
+			var activetab = getHashParameters()[gridid+"_f"];
+
+			// Loop through the filters at the top and store their details in the filters variable
+			$(element).find('ul.grid-filter>li>a').each(function(i,filterlink){
+				filters.push({
+					linkelement : filterlink,
+					name : $(filterlink).text(),
+					selected : $(filterlink).text() == activetab ? true : false
+				});
+				$(filterlink).on('click',handle_filterlink_click);
 			});
-		});
 
-		if ( filters.filter(function(f){return f.selected}).length == 0 ) {
-			filters[0].selected = true;
-		}
+			// If none of the filters was chosen as selected, mark the first as selected as a default.
+			if ( filters.filter(function(f){return f.selected}).length == 0 ) {
+				filters[0].selected = true;
+			}
 
-		console.log(filters);
+		};
+
+		var handle_filterlink_click = function(e){
+			e.stopPropagation();
+
+			$.each(filters,function(i,filter){
+				filter.selected = $(e.target).text() == filter.name ? true : false;
+			});
+
+			filterable_grid.update();
+		};
+
+		filterable_grid.update = function(){
+			$.each(filters,function(i,filter){
+				if ( filter.selected ) {
+					$(filter.linkelement).attr('aria-selected',true);
+					$(filter.linkelement).addClass('grid-filter-selected');
+				} else {
+					$(filter.linkelement).removeAttr('aria-selected');
+					$(filter.linkelement).removeClass('grid-filter-selected');
+				}
+			});
+		};
 
 
-		return this;
+		init_filters();
+		filterable_grid.update();
+		return filterable_grid;
 	};
 
 
