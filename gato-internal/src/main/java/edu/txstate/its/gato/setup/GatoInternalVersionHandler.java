93ab68c61bb8d1b4782023cb6a79ddc3a0de010c
Anne Faber <anne.faber@txstate.edu>
2017-08-29 10:47:24 -0500

    Restore the radio buttons that allow the user to select whether they want to put a new component at the beginning or end of an area

--- gato-internal/src/main/java/edu/txstate/its/gato/setup/GatoInternalVersionHandler.java
+++ gato-internal/src/main/java/edu/txstate/its/gato/setup/GatoInternalVersionHandler.java
@@ -141,6 +141,7 @@ public class GatoInternalVersionHandler extends DefaultModuleVersionHandler {
       //.addTask(new FindAndChangeTemplateIdTask(RepositoryConstants.WEBSITE, "gato-template-tsus:pages/home", "gato-template-tsus2017:pages/home"))
       .addTask(new FindAndChangeTemplateIdTask(RepositoryConstants.WEBSITE, "gato-template-tsus:pages/standard", "gato-template-tsus2017:pages/standard"))
       .addTask(new FindAndChangeTemplateIdTask(RepositoryConstants.WEBSITE, "gato-template-tsus:pages/mail", "gato-template-tsus2017:pages/form"))
+      .addTask(new BootstrapSingleModuleResource("config.modules.pages.dialogs.newComponent.form.tabs.components.fields.mgnl-position.xml"))
       .addTasks(installOrUpdateTasks())
     );
   }
