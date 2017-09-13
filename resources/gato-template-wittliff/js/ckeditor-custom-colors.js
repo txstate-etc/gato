70552f88cc56f59282e4e70aa0e044b91de6cd17
Anne Faber <anne.faber@txstate.edu>
2017-09-13 10:11:25 -0500

    Add Wittliff colors to rich editor tables and color picker

---
+++ resources/gato-template-wittliff/js/ckeditor-custom-colors.js
@@ -0,0 +1,22 @@
+window.cktemplatevars = {
+    definition_colors: "222222,465147,654D3C,7D2F19,764A33,b30e1b",
+    header_color_options: [ ['None', 'header-color-none'],
+                         [ 'Default (Coffee)', 'header-color-coffee' ],
+                         [ 'Cactus', 'header-color-cactus' ],
+                         [ 'Tumbleweed', 'header-color-tumbleweed'],
+                         [ 'Silver', 'header-color-silver'],
+                         [ 'Sienna', 'header-color-sienna' ],
+                         [ 'Pale Brown', 'header-color-palebrown'],
+                         [ 'Maize', 'header-color-maize']
+                         ],
+    default_header_color_class: 'header-color-coffee',
+    template_colors: [ ['Cactus', '#465147' ],
+                                          ['Coffee', '#654D3C'],
+                                          ['Tumbleweed', '#C69D77'],
+                                          ['Silver', '#C8BEB7'],
+                                          ['Sienna', '#7D2F19'],
+                                          ['Pale Brown', '#764A33'],
+                                          ['Maize', '#F3C9A3']
+                     ],
+    template_color_label: "Wittliff Colors"
+}
