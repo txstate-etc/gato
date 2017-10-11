d03c2a1ad30b4a5a0615aa1e466b66a7647d987f
Nick Wing <nwing@txstate.edu>
2017-10-10 21:13:07 -0500

    social media upgrade task should not run again, we only want to delete the crufty ones left by last month upgrade process

--- gato-internal/src/main/java/edu/txstate/its/gato/setup/UpgradeSocialmediaTask.java
+++ gato-internal/src/main/java/edu/txstate/its/gato/setup/UpgradeSocialmediaTask.java
@@ -36,41 +36,18 @@ public class UpgradeSocialmediaTask extends GatoBaseUpgradeTask {
     String areaname="socialmedia";
 
     visitPages(session, page -> {
-
-        if (page.hasNode(areaname)) {
-          log.warn("Processing socialmedia upgrade node opertion on : "+page.getName());
-
-          Node socialmediaArea = page.getNode(areaname);
-          if (socialmediaArea.hasNode("importSocialLink")) {
-            Node importSocialLinkNode = socialmediaArea.getNode("importSocialLink");
-            if (importSocialLinkNode.hasNode("sociallinks")) {
-              Node sociallinksNode = importSocialLinkNode.getNode("sociallinks");
-              if (!sociallinksNode.hasNodes()) {
-                importSocialLinkNode.remove();
-              }
-            }
-            return;
-          }
-
-          Node socialComponent = NodeUtil.createPath(socialmediaArea, "importSocialLink", NodeTypes.Component.NAME);
-          socialComponent.setProperty("mgnl:template", "gato-template:components/sociallink");
-          Node newSocialNode = NodeUtil.createPath(socialComponent, "sociallinks", NodeTypes.ContentNode.NAME);
-
-          NodeIterator nodeiter = socialmediaArea.getNodes();
-          while (nodeiter.hasNext()) {
-            Node oldSocialItem = nodeiter.nextNode();
-            if (oldSocialItem.getName()!="importSocialLink") {
-              Node socialItem = NodeUtil.createPath(newSocialNode, oldSocialItem.getName(), NodeTypes.ContentNode.NAME);
-              PropertyIterator iter = oldSocialItem.getProperties();
-              while (iter.hasNext()){
-                Property p = iter.nextProperty();
-                if (!p.getName().startsWith("jcr:") && !p.getName().startsWith("mgnl:"))
-                  socialItem.setProperty(p.getName(),p.getValue());
-              }
-              log.warn("deleting deleting node  "+NodeUtil.getName(oldSocialItem)+" from area "+ socialmediaArea.getName());
-              oldSocialItem.remove();
+      if (page.hasNode(areaname)) {
+        Node socialmediaArea = page.getNode(areaname);
+        if (socialmediaArea.hasNode("importSocialLink")) {
+          Node importSocialLinkNode = socialmediaArea.getNode("importSocialLink");
+          if (importSocialLinkNode.hasNode("sociallinks")) {
+            Node sociallinksNode = importSocialLinkNode.getNode("sociallinks");
+            if (!sociallinksNode.hasNodes()) {
+              log.warn("Removing empty socialmedia area at : "+importSocialLinkNode.getPath());
+              importSocialLinkNode.remove();
             }
           }
+        }
       }
     });
   }
