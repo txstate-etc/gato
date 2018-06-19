b7df25d0f5313aec630b3a1acc5cc4d0b89b2a51
Anne Faber <anne.faber@txstate.edu>
2018-06-19 09:27:47 -0500

    use different logic for header transitions. animate using transforms instead of left and top

--- resources/gato-template-mobilefirst/js/mobilefirst.js
+++ resources/gato-template-mobilefirst/js/mobilefirst.js
@@ -5,49 +5,90 @@ jQuery(document).ready(function($) {
   magnolialabelchange('.fullwidth .content_add', '.mgnlEditor.mgnlPlaceholder', 'Add Content');
   magnolialabelchange('.fullwidth .image_add', '.mgnlEditor.mgnlPlaceholder', 'Add Image');
 
-  function animateHeader() {
-    var header = $('header');
-    var banner = $('.banner-section');
-    var menubar = $('.menubar');
-    var logoblock = $('.logo-block');
-    var searchButton = $('.btn-search')
-    var animationDuration = 300
+  var header = $('header');
+  var banner = $('.banner-section');
+  var menubar = $('.menubar');
+  var logoblock = $('.logo-block');
+  var searchButton = $('.btn-search')
+  var animationDuration = 300
+
+  function goToScrollState() {
+    //search button to top, logo block up, menubar down
+    searchButton.velocity({translateX: "60px", translateY: "-60px"},
+     {duration: animationDuration,
+     begin: function(){
+       header.addClass("animatetoscroll")
+     },
+     complete: function() {
+       header.removeClass("animatetoscroll")
+     }})
+
+     logoblock.velocity({translateY: "-120px"},
+      {duration: animationDuration,
+       complete: function() {
+         logoblock.css('display', 'none')
+       }})
+
+     menubar.velocity({translateY: "60px"},
+      {duration: animationDuration,
+       begin: function() {
+         menubar.css('display', 'block')
+       }})
+  }
+
+  function goToTopState() {
+    //search button to side, logo block down, menubar up
+      searchButton.velocity({translateX: "0px", translateY: "0px"},
+        {duration: animationDuration,
+         begin: function() {
+           header.addClass("animatetotop")
+         },
+         complete: function() {
+           header.removeClass('animatetotop')
+         }})
+
+      logoblock.velocity({translateY: "0px"},
+        {duration: animationDuration,
+         begin: function() {
+           logoblock.css('display', 'flex')
+         }})
+
+       menubar.velocity({translateY: "0px"},
+        {duration: animationDuration,
+         complete: function() {
+           menubar.css('display', 'none')
+         }})
+  }
+
+  function stopHeaderAnimation() {
+    searchButton.velocity("stop")
+    logoblock.velocity("stop")
+    menubar.velocity("stop")
+    header.removeClass('animatetotop').removeClass('animatetoscroll')
+  }
+
+  $(window).scroll(function() {
     if (header.offset().top > 0 ) {
       banner.addClass('scrolled');
-      if (!searchButton.is('.scrolled') && !searchButton.is('.velocity-animating'))
-        searchButton.velocity({left: [60, 0], top: [0, 60]}, {duration: animationDuration, complete: function(){
-          searchButton.addClass('scrolled')
-        }})
-      if(!logoblock.is('.scrolled') && !logoblock.is('.velocity-animating'))
-        logoblock.velocity({top: [-120, 0]}, {duration: animationDuration, complete: function(){
-          logoblock.addClass('scrolled')
-        }})
-      if (!menubar.is('.scrolled') && !menubar.is('.velocity-animating'))
-        menubar.velocity({top: [0, -60]}, {duration: animationDuration, begin: function(){
-          menubar.addClass('scrolled')
-        }})
+      if (header.is('.animatetotop')) {
+        stopHeaderAnimation();
+      }
+      if (!header.is('.scrolled')) {
+        goToScrollState()
+        header.addClass('scrolled')
+      }
     }
     else {
       banner.removeClass('scrolled');
-      if (searchButton.is('.scrolled') && !searchButton.is('.velocity-animating'))
-        searchButton.velocity({left: [0, 60], top: [60, 0]}, {duration: animationDuration, complete: function(){
-          searchButton.removeClass('scrolled')
-        }})
-      if (logoblock.is('.scrolled') && !logoblock.is('.velocity-animating')){
-        logoblock.velocity({top: [0, -120]}, {duration: animationDuration, begin: function(){
-          logoblock.removeClass('scrolled')
-        }})
+      if (header.is('.animatetoscroll')) {
+        stopHeaderAnimation();
       }
-      if (menubar.is('.scrolled') && !menubar.is('.velocity-animating')) {
-        menubar.velocity({top: [-60, 0]}, {duration: animationDuration, complete: function() {
-          menubar.removeClass('scrolled')
-        }})
+      if (header.is('.scrolled')) {
+        goToTopState()
+        header.removeClass('scrolled')
       }
-
     }
-  }
-
-  $(window).scroll(animateHeader)
+  })
 
   $('.footer-column-title').click(function(e) {
     e.preventDefault();
