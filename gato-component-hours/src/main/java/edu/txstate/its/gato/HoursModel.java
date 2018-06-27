74454c27ddec13681d6207fc098170ffb691b8fa
Nick Wing <nwing@txstate.edu>
2018-06-27 16:41:33 -0500

    reworked gato-component-hours to pull from trumba

--- gato-component-hours/src/main/java/edu/txstate/its/gato/HoursModel.java
+++ gato-component-hours/src/main/java/edu/txstate/its/gato/HoursModel.java
@@ -1,357 +1,273 @@
 package edu.txstate.its.gato;
 
 import info.magnolia.rendering.model.RenderingModel;
-import info.magnolia.rendering.model.RenderingModelImpl;
 import info.magnolia.rendering.template.RenderableDefinition;
 import javax.inject.Inject;
 import javax.jcr.Node;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
-import java.io.UnsupportedEncodingException;
 import java.util.Calendar;
 import java.text.SimpleDateFormat;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.TimeZone;
 import java.util.Locale;
 import org.apache.commons.lang3.time.DateUtils;
 import org.apache.commons.lang3.StringUtils;
 
 
-public class HoursModel<RD extends RenderableDefinition> extends RenderingModelImpl<RD> {
+public class HoursModel extends TrumbaEventModel {
+  protected final DateFormat jsonFormat = new SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ss" );
 
-    public static final String CALENDAR_URL = "http://etcalendar.its.txstate.edu";
-    DateFormat inputFormatTimed = new SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ss" );
-    DateFormat inputFormatUntimed = new SimpleDateFormat( "yyyyMMdd" );
-    public Document rssDocument;
+  @Inject
+  public HoursModel(Node content, RenderableDefinition definition, RenderingModel parent) {
+    super(content, definition, parent);
+  }
 
-    @Inject
-    public HoursModel(Node content, RD definition, RenderingModel<?> parent) {
-        super(content, definition, parent);
-        try {
-          String url = constructUrl(true, content.getProperty("calendarId").getString());
-          System.out.println("<!-- USING URL : "+ url + "-->");
-          rssDocument = DomUtils.parseXml( url );
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
+  public String itemHtml( EventItem item ) {
+    Date startDate = item.getStartDate();
+    Date endDate = item.getEndDate();
+
+    String itemHtml = "";
+
+    itemHtml += "<div class=\"hours\">";
+    itemHtml += "<h4>" + item.getTitle() + "</h4>";
+    itemHtml += "<div class=\"times\">";
+
+    if (item.isCancelled()) {
+      itemHtml += "closed";
+    } else if (startDate.getHours() == endDate.getHours() && startDate.getMinutes() == endDate.getMinutes()) {
+      itemHtml += "24 hours";
+    } else {
+      itemHtml += formatTime(startDate);
+      itemHtml += " - ";
+      itemHtml += formatTime(endDate);
     }
 
-    public String itemHtml( Element item ) throws UnsupportedEncodingException {
-        String title = getTitle( item ).trim();
+    itemHtml += "</div>";
+    itemHtml += "</div>";
 
-        Date startDate = getDate( item, "starttime" );
-        Date endDate = getDate( item, "endtime" );
+    return itemHtml;
+  }
 
-        String itemHtml = "";
+  public String itemJson( EventItem item ) {
+    String title = item.getTitle();
+    Date startDate = item.getStartDate();
+    Date endDate = item.getEndDate();
 
-        itemHtml += "<div class=\"hours\">";
-        itemHtml += "<h4>" + title + "</h4>";
-        itemHtml += "<div class=\"times\">";
+    String json = "";
 
-        if (isCancelled(item)) {
-            itemHtml += "closed";
-        } else if (startDate.getHours() == endDate.getHours() && startDate.getMinutes() == endDate.getMinutes()) {
-            itemHtml += "24 hours";
-        } else {
-            itemHtml += formatTime(startDate);
-            itemHtml += " - ";
-            itemHtml += formatTime(endDate);
-        }
+    json += "{";
+    json += "\"title\":\"" + title.replace("\"", "\\\"") + "\",";
 
-        itemHtml += "</div>";
-        itemHtml += "</div>";
+    if (item.isCancelled()) {
+      json += "\"className\":\"canceled\",";
+    }
+    json += "\"start\":\""+jsonFormat.format(startDate)+"\",";
+    json += "\"end\":\""+jsonFormat.format(endDate)+"\"";
 
-        return itemHtml;
+    json += "}";
+    return json;
+  }
+
+  private boolean isBetween(Date now, Date start, Date end) {
+    Calendar nowc = Calendar.getInstance();
+    nowc.setTime(now);
+    Calendar startc = Calendar.getInstance();
+    startc.setTime(start);
+    Calendar endc = Calendar.getInstance();
+    endc.setTime(end);
+    return nowc.before(endc) && nowc.after(startc);
+  }
+
+  private boolean isToday(Date d) {
+    Calendar dc = Calendar.getInstance();
+    dc.setTime(d);
+    Calendar nowc = Calendar.getInstance();
+    return DateUtils.isSameDay(dc, nowc);
+  }
+
+  private String formatDate(Date date, boolean fullDate) {
+    SimpleDateFormat df;
+    if (fullDate) {
+      df = new SimpleDateFormat("MMMM d");
+    } else {
+      df = new SimpleDateFormat("EEEE");
+    }
+    return df.format(date);
+  }
+
+  private String formatTime(Date date) {
+    SimpleDateFormat df;
+    if (date.getMinutes() > 0) {
+      df = new SimpleDateFormat("h:mma");
+    } else {
+      df = new SimpleDateFormat("ha");
+    }
+    return df.format(date).toLowerCase(Locale.ENGLISH);
+  }
+
+  private String formatOpen(Date dateClose, Date nextOpen) {
+    if (nextOpen != null && dateClose.equals(nextOpen)) {
+      return "Open 24hrs";
+    }
+    return "Open until " + formatTime(dateClose);
+  }
+
+  private String formatClosed(Date nextOpen) {
+    String time = "Closed";
+    if (nextOpen != null) {
+      time += " until " + formatTime(nextOpen);
+      Calendar cutoff = Calendar.getInstance();
+      cutoff.add( Calendar.DATE, 1 );
+      if (nextOpen.after(cutoff.getTime())) {
+        cutoff.add( Calendar.DATE, 6 );
+        boolean fullDate = nextOpen.after(cutoff.getTime());
+        time += fullDate ? ", " : " ";
+        time += formatDate(nextOpen, fullDate);
+      }
+    }
+    return time;
+  }
+
+  // Examples:
+  // "Open until 3am"
+  // "Open 24hrs"
+  // "Closed until 7am"
+  // "Closed until 7am Monday"
+  // "Closed until 7am, January 3"
+  // "Closed" (Shouldn't ever happen)
+  private String mobileHoursHtml(List<EventItem> items) {
+    String html = "";
+    Date now = Calendar.getInstance().getTime();
+    Date firstOpen = null;
+    Date firstClose = null;
+    Date secondOpen = null;
+    Date secondClose = null;
+    Date thirdOpen = null;
+    Date thirdClose = null;
+
+    // Find the next three open/close times (starting yesterday)
+    for ( EventItem item : items ) {
+      // FIXME: don't hardcode the title, put it in the JCR (but default it to this)
+      if (!item.getTitle().equalsIgnoreCase("Alkek Library") || item.isCancelled()) {
+        continue;
+      }
+
+      if (firstOpen == null) {
+        firstOpen = item.getStartDate();
+        firstClose = item.getEndDate();
+      } else if (secondOpen == null) {
+        secondOpen = item.getStartDate();
+        secondClose = item.getEndDate();
+      } else {
+        thirdOpen = item.getStartDate();
+        thirdClose = item.getEndDate();
+        break;
+      }
     }
 
-    public String itemJson( Element item ) throws UnsupportedEncodingException {
-        String title = getTitle( item ).trim();
+    // Timeline of the three openings and the various states
+    // we can be in relative to the current time.
+    // Usually the 3 openings are yesterday, today, and tomorrow,
+    // but may be different during holidays, etc.
+    //   a  b  c  d  e  f  g
+    // <---###---###---###--->
+    //      1     2     3
 
-        Date startDate = getDate( item, "starttime" );
-        Date endDate = getDate( item, "endtime" );
-
-        String json = "";
-
-        json += "{";
-        json += "\"title\":\"" + title.replace("\"", "\\\"") + "\",";
-
-        if (isCancelled(item)) {
-            json += "\"className\":\"canceled\",";
-        }
-        json += "\"start\":\""+inputFormatTimed.format(startDate)+"\",";
-        json += "\"end\":\""+inputFormatTimed.format(endDate)+"\"";
-
-        json += "}";
-        return json;
+    if (firstOpen == null) {
+      // a'. No known times; maybe they stopped entering them? Just return blank
+      return "";
     }
 
-    private Date getDate( Element item, String dateLabel ) {
-
-        String dateString = DomUtils.getTextValue( item, dateLabel );
-        try {
-        inputFormatTimed.setTimeZone(TimeZone.getTimeZone("America/Chicago"));
-            return inputFormatTimed.parse( dateString );
-        } catch ( java.text.ParseException e ) {
-            // couldn't parse with a date
-        }
-
-        try {
-            return inputFormatUntimed.parse( dateString );
-        } catch ( java.text.ParseException e ) {
-            // couldn't parse without a date
-        }
-    return null;
+    if (firstOpen.after(now)) {
+      // a. closed now, return next opening
+      return formatClosed(firstOpen);
     }
 
-    private boolean isBetween(Date now, Date start, Date end) {
-        Calendar nowc = Calendar.getInstance();
-        nowc.setTime(now);
-        Calendar startc = Calendar.getInstance();
-        startc.setTime(start);
-        Calendar endc = Calendar.getInstance();
-        endc.setTime(end);
-        return nowc.before(endc) && nowc.after(startc);
+    if (firstOpen.before(now) && firstClose.after(now)) {
+      // b. open now. return "Open until hh:mm p"
+      return formatOpen(firstClose, secondOpen);
     }
 
-    private boolean isToday(Date d) {
-        Calendar dc = Calendar.getInstance();
-        dc.setTime(d);
-        Calendar nowc = Calendar.getInstance();
-        return DateUtils.isSameDay(dc, nowc);
+    if (secondOpen == null || secondOpen.after(now)) {
+      // c. closed now, return next opening
+      return formatClosed(secondOpen);
     }
 
-    private boolean isCancelled ( Element item ) {
-        boolean isCancelled = "cancelled".equals( DomUtils.getTextValue( item, "status" ).toLowerCase() );
-        return isCancelled;
+    if (secondOpen.before(now) && secondClose.after(now)) {
+      // d. open now. return "Open until hh:mm p"
+      return formatOpen(secondClose, thirdOpen);
     }
 
-    private String getTitle ( Element item ) {
-        return DomUtils.getTextValue( item, "title" );
+    if (thirdOpen == null || thirdOpen.after(now)) {
+      // e. closed now, return next opening
+      return formatClosed(thirdOpen);
     }
 
-    private String formatDate(Date date, boolean fullDate) {
-        SimpleDateFormat df;
-        if (fullDate) {
-            df = new SimpleDateFormat("MMMM d");
-        } else {
-            df = new SimpleDateFormat("EEEE");
-        }
-        return df.format(date);
+    // The next two states should never happen?
+    // It would mean there were two events on the same day
+
+    if (thirdOpen.before(now) && thirdClose.after(now)) {
+      // f. open now. return "Open until hh:mm p"
+      return formatOpen(thirdClose, null);
     }
 
-    private String formatTime(Date date) {
-        SimpleDateFormat df;
-        if (date.getMinutes() > 0) {
-            df = new SimpleDateFormat("h:mma");
-        } else {
-            df = new SimpleDateFormat("ha");
-        }
-        return df.format(date).toLowerCase(Locale.ENGLISH);
+    if (thirdClose.before(now)) {
+      // g. return 'closed'
+      return formatClosed(null);
     }
 
-    private String formatOpen(Date dateClose, Date nextOpen) {
-        if (nextOpen != null && dateClose.equals(nextOpen)) {
-            return "Open 24hrs";
+    // really should never happen
+    return "";
+  }
+
+  private String constructUrl (Boolean mobileDevice, String calendarId) {
+    String url = baseUrl(content, "1411153");
+
+    Calendar startDate = Calendar.getInstance();
+    Calendar endDate = Calendar.getInstance();
+    startDate.add( Calendar.DATE, -1 );
+    endDate.add( Calendar.DATE, 94 );
+    endDate.set( Calendar.DAY_OF_MONTH, 1 );
+    endDate.add( Calendar.DATE, -1);
+
+    url = url + "?startdate=" + trumbaformat.format(startDate.getTime());
+    url = url + "&enddate=" + trumbaformat.format(endDate.getTime());
+    return url;
+  }
+
+  public String getEvents(Boolean isMobile){
+    if (this.items.size() == 0) return "No hours data.";
+    String out="";
+    if(isMobile){
+      out = mobileHoursHtml(this.items);
+    } else {
+      Date now = new Date();
+      for ( EventItem item : (List<EventItem>) this.items) {
+        if (isToday(item.getStartDate())) {
+          out += itemHtml( item );
         }
-        return "Open until " + formatTime(dateClose);
+      }
     }
+    return out;
+  }
 
-    private String formatClosed(Date nextOpen) {
-        String time = "Closed";
-        if (nextOpen != null) {
-            time += " until " + formatTime(nextOpen);
-            Calendar cutoff = Calendar.getInstance();
-            cutoff.add( Calendar.DATE, 1 );
-            if (nextOpen.after(cutoff.getTime())) {
-                cutoff.add( Calendar.DATE, 6 );
-                boolean fullDate = nextOpen.after(cutoff.getTime());
-                time += fullDate ? ", " : " ";
-                time += formatDate(nextOpen, fullDate);
-            }
-        }
-        return time;
+  public String getFullCalendar(String calendarId){
+    String out = "[";
+    boolean firstrun = true;
+    for ( EventItem item : (List<EventItem>) this.items ) {
+      if (!firstrun) out += ",";
+      out += itemJson( item );
+      firstrun = false;
     }
+    return out+"]";
+  }
 
-    // Examples:
-    // "Open until 3am"
-    // "Open 24hrs"
-    // "Closed until 7am"
-    // "Closed until 7am Monday"
-    // "Closed until 7am, January 3"
-    // "Closed" (Shouldn't ever happen)
-    private String mobileHoursHtml(NodeList items) {
-        String html = "";
-        Date now = Calendar.getInstance().getTime();
-        Date firstOpen = null;
-        Date firstClose = null;
-        Date secondOpen = null;
-        Date secondClose = null;
-        Date thirdOpen = null;
-        Date thirdClose = null;
-
-        // Find the next three open/close times (starting yesterday)
-        for ( int i = 0; i < items.getLength(); i++ ) {
-            Element item = (Element)items.item(i);
-            String title = getTitle( item ).trim();
-
-            // FIXME: don't hardcode the title, put it in the JCR (but default it to this)
-            if (!title.equalsIgnoreCase("Alkek Library") || isCancelled(item)) {
-                continue;
-            }
-
-            if (firstOpen == null) {
-                firstOpen = getDate( item, "starttime" );
-                firstClose = getDate( item, "endtime" );
-            } else if (secondOpen == null) {
-                secondOpen = getDate( item, "starttime" );
-                secondClose = getDate( item, "endtime" );
-            } else {
-                thirdOpen = getDate( item, "starttime" );
-                thirdClose = getDate( item, "endtime" );
-                break;
-            }
-        }
-
-        // Timeline of the three openings and the various states
-        // we can be in relative to the current time.
-        // Usually the 3 openings are yesterday, today, and tomorrow,
-        // but may be different during holidays, etc.
-        //   a  b  c  d  e  f  g
-        // <---###---###---###--->
-        //      1     2     3
-
-        if (firstOpen == null) {
-            // a'. No known times; maybe they stopped entering them? Just return blank
-            return "";
-        }
-
-        if (firstOpen.after(now)) {
-            // a. closed now, return next opening
-            return formatClosed(firstOpen);
-        }
-
-        if (firstOpen.before(now) && firstClose.after(now)) {
-            // b. open now. return "Open until hh:mm p"
-            return formatOpen(firstClose, secondOpen);
-        }
-
-        if (secondOpen == null || secondOpen.after(now)) {
-            // c. closed now, return next opening
-            return formatClosed(secondOpen);
-        }
-
-        if (secondOpen.before(now) && secondClose.after(now)) {
-            // d. open now. return "Open until hh:mm p"
-            return formatOpen(secondClose, thirdOpen);
-        }
-
-        if (thirdOpen == null || thirdOpen.after(now)) {
-            // e. closed now, return next opening
-            return formatClosed(thirdOpen);
-        }
-
-        // The next two states should never happen?
-        // It would mean there were two events on the same day
-
-        if (thirdOpen.before(now) && thirdClose.after(now)) {
-            // f. open now. return "Open until hh:mm p"
-            return formatOpen(thirdClose, null);
-        }
-
-        if (thirdClose.before(now)) {
-            // g. return 'closed'
-            return formatClosed(null);
-        }
-
-        // really should never happen
-        return "";
-    }
-
-    private String constructUrl (Boolean mobileDevice, String calendarId) throws UnsupportedEncodingException {
-        String url = CALENDAR_URL + "/search.xml?b=de&path="+java.net.URLEncoder.encode(calendarId, "UTF-8");
-
-        Calendar startDate = Calendar.getInstance();
-        Calendar endDate = Calendar.getInstance();
-        if ( mobileDevice ) {
-            startDate.add( Calendar.DATE, -1 );
-            endDate.add( Calendar.DATE, 94 );
-            endDate.set( Calendar.DAY_OF_MONTH, 1 );
-            endDate.add( Calendar.DATE, -1);
-        }
-
-        url = url + "&year=" + startDate.get( Calendar.YEAR );
-        url = url + "&month=" + ( startDate.get( Calendar.MONTH ) + 1 );
-        url = url + "&day=" + startDate.get( Calendar.DATE );
-
-        url = url + "&end_year=" + endDate.get( Calendar.YEAR );
-        url = url + "&end_month=" + ( endDate.get( Calendar.MONTH ) + 1 );
-        url = url + "&end_day=" + endDate.get( Calendar.DATE );
-
-        return url;
-    }
-
-
-    public String getEvents(Boolean isMobile){
-        String out="";
-        try{
-            if(rssDocument != null){
-                Element root = rssDocument.getDocumentElement();
-                NodeList items = root.getElementsByTagName( "object");
-                if(isMobile){
-                    out = mobileHoursHtml(items);
-                }
-                else{
-                    Date now = new Date();
-                    for ( int i = 0; i < items.getLength(); i++ ) {
-                      Element thisItem = (Element)items.item(i);
-                      if (isToday(getDate(thisItem, "starttime"))) {
-                        out += itemHtml( thisItem );
-                      }
-                    }
-                }
-                if (items.getLength() == 0) out = "No hours data.";
-
-            }
-            else{
-                out = "There was an error retrieving hours.";
-            }
-            return out;
-        }
-        catch(UnsupportedEncodingException e){
-            e.printStackTrace();
-            return "There was an error retrieving hours.";
-        }
-    }
-
-    public String getFullCalendar(String calendarId){
-        String out="";
-        try{
-            if(rssDocument != null){
-                Element root = rssDocument.getDocumentElement();
-                NodeList items = root.getElementsByTagName( "object");
-                out += "[";
-                for ( int i = 0; i < items.getLength(); i++ ) {
-                    if (i > 0) out += ",";
-                    Element thisItem = (Element)items.item(i);
-                    out += itemJson( thisItem );
-                }
-                out += "]";
-            }
-            else{
-                out = "[]";
-            }
-            return out;
-        }
-        catch(UnsupportedEncodingException e){
-            e.printStackTrace();
-            return "[]";
-        }
-    }
-
-    public boolean showEvent(Calendar endDate){
-        return (endDate.getTimeInMillis() > Calendar.getInstance().getTimeInMillis());
-    }
+  public boolean showEvent(Calendar endDate){
+      return (endDate.getTimeInMillis() > Calendar.getInstance().getTimeInMillis());
+  }
 
 }
