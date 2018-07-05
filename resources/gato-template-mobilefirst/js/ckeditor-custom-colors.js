008cf9c634213925c567ea6a70db2cbff3e00e90
Anne Faber <anne.faber@txstate.edu>
2018-07-05 13:33:26 -0500

    Remove river, blue, and old gold from mobile first color choices. Add config file for rich editor table header colors

---
+++ resources/gato-template-mobilefirst/js/ckeditor-custom-colors.js
@@ -0,0 +1,16 @@
+window.cktemplatevars = {
+    definition_colors: "222222,501214,6a5638,363534,b30e1b",
+    header_color_options: [ ['None', 'header-color-none'],
+                         [ 'Default (Gold)', 'header-color-gold' ],
+                         [ 'Maroon', 'header-color-maroon' ],
+                         [ 'Charcoal','header-color-charcoal' ],
+                         [ 'Sandstone', 'header-color-sandstone' ]
+                         ],
+    default_header_color_class: 'header-color-gold',
+    template_colors: [ ['Maroon', '#501214' ],
+                                          ['Gold', '#6A5638'],
+                                          ['Charcoal', '#363534'],
+                                          ['Sandstone', '#E8E3DB']
+                     ],
+    template_color_label: "Texas State Colors"
+}
--- resources/gato-template-mobilefirst/js/color-picker-config.js
+++ resources/gato-template-mobilefirst/js/color-picker-config.js
@@ -1,7 +1,7 @@
 {
     "default": {
         "singleColor": true,
-        "colors": ["color1", "color2", "color3", "color4", "color5", "color6", "color7"]
+        "colors": ["color1", "color2", "color3", "color4"]
     },
     "iconText": {
         "singleColor": true,
@@ -9,34 +9,34 @@
     },
     "gradientButton": {
         "singleColor": true,
-        "colors": ["color1", "color2", "color3", "color4", "color5", "color6", "color7"]
+        "colors": ["color1", "color2", "color3", "color4"]
     },
     "rolloverButton": {
         "singleColor": true,
-        "colors": ["color1", "color2", "color3", "color4", "color7"]
+        "colors": ["color1", "color2", "color3"]
     },
     "reverseButton": {
         "singleColor": true,
-        "colors": ["color1", "color2", "color3", "color4"]
+        "colors": ["color1", "color2", "color3"]
     },
     "threeDButton": {
         "singleColor": true,
-        "colors": ["color1", "color2", "color3", "color4", "color5", "color6", "color7"]
+        "colors": ["color1", "color2", "color3", "color4"]
     },
     "cardFilters": {
         "singleColor": true,
-        "colors": ["color1", "color2", "color3", "color6"]
+        "colors": ["color1", "color2", "color3", "color4"]
     },
     "imageCard": {
         "singleColor": true,
-        "colors": ["color1", "color2", "color3", "color6"]
+        "colors": ["color1", "color2", "color3", "color4"]
     },
     "slider": {
         "singleColor": false,
-        "colors": ["color1", "color2", "color3", "color4", "color5", "color6", "color7"]
+        "colors": ["color1", "color2", "color3", "color4"]
     },
     "txstateHomeTopSlider": {
         "singleColor": true,
-        "colors": ["color1", "color2", "color3", "color4", "color5", "color6", "color7"]
+        "colors": ["color1", "color2", "color3", "color4"]
     }
 }
