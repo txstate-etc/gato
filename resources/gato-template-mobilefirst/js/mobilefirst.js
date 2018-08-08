60134bfb349dffafca2d0bbfb85fd4b53c380874
Anne Faber <anne.faber@txstate.edu>
2018-08-08 10:24:31 -0500

    Mobile First: Fixing some display problems in IE. Other CSS tweaks based on marketing feedback

--- resources/gato-template-mobilefirst/js/mobilefirst.js
+++ resources/gato-template-mobilefirst/js/mobilefirst.js
@@ -127,4 +127,9 @@ jQuery(document).ready(function($) {
       })
     }
   })
+
+  $(document).ready(function() {
+    var $patternImages = $('.mobilefirst-pattern .pattern-image img')
+    objectFitImages($patternImages);
+  });
 });
