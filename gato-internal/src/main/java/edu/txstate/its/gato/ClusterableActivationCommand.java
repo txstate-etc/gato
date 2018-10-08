13c8004ce3811ebfc39eb2450a2d7bca3d65cb4f
Anne Faber <anne.faber@txstate.edu>
2018-10-08 09:21:36 -0500

    rewrote using publishing module instead of activation. Not tested yet

--- gato-internal/src/main/java/edu/txstate/its/gato/ClusterableActivationCommand.java
+++ gato-internal/src/main/java/edu/txstate/its/gato/ClusterableActivationCommand.java
@@ -1,45 +1,52 @@
 package edu.txstate.its.gato;
 
-import info.magnolia.cms.core.Content;
-import info.magnolia.cms.util.ContentUtil;
+import info.magnolia.cms.core.version.VersionManager;
 import info.magnolia.context.Context;
-import info.magnolia.jcr.util.PropertyUtil;
-import info.magnolia.module.activation.commands.ActivationCommand;
+import info.magnolia.init.MagnoliaConfigurationProperties;
+import info.magnolia.objectfactory.ComponentProvider;
+import info.magnolia.objectfactory.Components;
+import info.magnolia.publishing.command.PublicationCommand;
+import info.magnolia.publishing.operation.SendOperation;
+import info.magnolia.publishing.sender.Sender;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.jcr.Node;
-import javax.jcr.InvalidItemStateException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ClusterableActivationCommand extends ActivationCommand {
-  private static final Logger log = LoggerFactory.getLogger(ClusterableActivationCommand.class);
-	@Override
-	public boolean execute(Context ctx) throws Exception {
-	  Node node = getJCRNode(ctx);
-		if (!node.hasProperty("gato_activated_on_creation")) {
-		  try {
-		    PropertyUtil.setProperty(node, "gato_activated_on_creation", Boolean.TRUE);
-		    node.getSession().save();
+public class ClusterableActivationCommand extends PublicationCommand {
+  private static final Logger log= LoggerFactory.getLogger(ClusterableActivationCommand.class);
 
-        // now that I know I'm the one doing the work, I need to make sure all
-        // ancestors are activated to be 100% sure this activation takes
-        for (int i = 1; i < node.getDepth(); i++) {
-          Content c = ContentUtil.asContent((Node)node.getAncestor(i));
-          try {
-            getSyndicator().activate(c.getParent().getHandle(), c, getOrderingInfo(c));
-          } catch (Exception e) {
-            log.error("aborting activation command, unable to automatically activate ancestor at path "+c.getHandle());
-            e.printStackTrace();
-            return false;
-          }
+  public ClusterableActivationCommand(VersionManager vm, ComponentProvider cp) {
+    super(vm, cp);
+  }
+
+  @Override
+  public boolean execute(Context ctx) throws Exception {
+    MagnoliaConfigurationProperties mcp = Components.getSingleton(MagnoliaConfigurationProperties.class);
+    if (!("true".equals(mcp.getProperty("gato.skipscheduler")))) {
+      Sender sender = getComponentProvider().newInstance(Sender.class, ctx, getComponentProvider());
+      Node toPublish = getJCRNode(ctx);
+      for (int i = 1; i < toPublish.getDepth(); i++) {
+        List<Node> ancestor = new ArrayList<>();
+        Node a = (Node) toPublish.getAncestor(i);
+        ancestor.add(a);
+        List<SendOperation.OperationResult> results = sender.publish(ancestor, getRule());
+        List<SendOperation.OperationResult> errors = results.stream().filter(result -> !result.isSuccess()).collect(Collectors.toList());
+        if (!errors.isEmpty()) {
+          log.error("aborting activation command, unable to automatically activate ancestor at path " + a.getPath());
+          return false;
         }
-
-        return super.execute(ctx);
-		  } catch (InvalidItemStateException e) {
-		    // another instance in the cluster beat us to it, we're cool with that
-		  }
-		}
-		return true;
-	}
+      }
+    }
+    else {
+      log.debug("Execute called, but skipscheduler is true. Doing nothing.");
+      return true;
+    }
+    return true;
+  }
 }
