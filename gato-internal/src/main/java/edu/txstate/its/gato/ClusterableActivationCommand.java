cbecc3ddfb1dc6552078ea2b51a0d8b2753122ff
Nick Wing <nwing@txstate.edu>
2018-11-29 11:04:31 -0600

    ClusterableActivationCommand was not set to ever activate the node in question, re-organized it

--- gato-internal/src/main/java/edu/txstate/its/gato/ClusterableActivationCommand.java
+++ gato-internal/src/main/java/edu/txstate/its/gato/ClusterableActivationCommand.java
@@ -21,26 +21,32 @@ import org.slf4j.LoggerFactory;
 public class ClusterableActivationCommand extends PublicationCommand {
   private static final Logger log= LoggerFactory.getLogger(ClusterableActivationCommand.class);
 
+  protected final ComponentProvider cp;
   public ClusterableActivationCommand(VersionManager vm, ComponentProvider cp) {
     super(vm, cp);
+    this.cp = cp;
   }
 
   @Override
   public boolean execute(Context ctx) throws Exception {
     MagnoliaConfigurationProperties mcp = Components.getSingleton(MagnoliaConfigurationProperties.class);
     if (!("true".equals(mcp.getProperty("gato.skipscheduler")))) {
-      Sender sender = getComponentProvider().newInstance(Sender.class, ctx, getComponentProvider());
+      Sender sender = getComponentProvider().newInstance(Sender.class, ctx, this.cp);
       Node toPublish = getJCRNode(ctx);
+      List<Node> publishList = new ArrayList<Node>();
       for (int i = 1; i <= toPublish.getDepth(); i++) {
-        List<Node> ancestor = new ArrayList<>();
         Node a = (Node) toPublish.getAncestor(i);
-        ancestor.add(a);
-        List<SendOperation.OperationResult> results = sender.publish(ancestor, getRule());
-        List<SendOperation.OperationResult> errors = results.stream().filter(result -> !result.isSuccess()).collect(Collectors.toList());
-        if (!errors.isEmpty()) {
-          log.error("aborting activation command, unable to automatically activate ancestor at path " + a.getPath());
-          return false;
-        }
+        publishList.add(a);
+      }
+      publishList.add(toPublish);
+
+      List<SendOperation.OperationResult> results = sender.publish(publishList, getRule());
+      List<SendOperation.OperationResult> errors = results.stream().filter(result -> !result.isSuccess()).collect(Collectors.toList());
+      if (!errors.isEmpty()) {
+        errors.forEach(result -> {
+          log.error("Receiver: {}, error: {}", result.getReceiverName(), result.getException().getMessage(), result.getException());
+        });
+        return false;
       }
     }
     else {
