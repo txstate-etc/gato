3790b6ef81a8cc8e5622f45c2330cb2546e5bb4a
Nick Wing <nwing@txstate.edu>
2018-12-11 17:25:02 -0600

    get full id from dacast links in embed video content type

--- gato-lib/src/main/java/edu/txstate/its/gato/GatoUtils.java
+++ gato-lib/src/main/java/edu/txstate/its/gato/GatoUtils.java
@@ -96,7 +96,7 @@ public final class GatoUtils {
   private static final Pattern EXTERNAL_LINK_PATTERN = Pattern.compile("^(\\w+:)?//.*$");
   private static final Pattern MEDIAFLO_ID_PATTERN = Pattern.compile("contentContainer_([a-f\\d\\-]+)");
   private static final Pattern MEDIAFLO_URL_PATTERN = Pattern.compile("^https?://mediaflo.*$");
-  private static final Pattern DACAST_URL_PATTERN = Pattern.compile("https?://.*dacast\\.com.*(\\d+[_/][a-z][_/]\\d+)");
+  private static final Pattern DACAST_URL_PATTERN = Pattern.compile("https?://.*dacast\\.com.*?(\\d+[_/][a-z][_/]\\d+)");
   private static final Pattern DACAST_SCRIPT_PATTERN = Pattern.compile("id=\"(\\d+[_/][a-z][_/]\\d+)\".*dacast\\.com");
   private static final Pattern JSONP_PATTERN = Pattern.compile("^\\s*\\w+\\((.+?)\\);?\\s*$");
   protected static final Pattern UUID_PATTERN = Pattern.compile("([a-f0-9]{8}-[a-f0-9]{4}-[1-5][a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12})(#[-\\w]+)?");
