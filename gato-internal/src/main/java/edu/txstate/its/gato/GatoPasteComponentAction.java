94c4f127163fca38046a548d8fdf42fb823ade2c
Ross McEwen <ram146@txstate.edu>
2019-04-10 11:31:45 -0500

    Paste Components: When pasting a component, the pasted component will appear immediately after the selected component instead of at the end of the section.

--- gato-internal/src/main/java/edu/txstate/its/gato/GatoPasteComponentAction.java
+++ gato-internal/src/main/java/edu/txstate/its/gato/GatoPasteComponentAction.java
@@ -13,25 +13,33 @@ import info.magnolia.ui.vaadin.gwt.client.shared.AbstractElement;
 import info.magnolia.ui.vaadin.gwt.client.shared.AreaElement;
 import info.magnolia.ui.vaadin.integration.jcr.JcrItemAdapter;
 import info.magnolia.ui.vaadin.overlay.MessageStyleTypeEnum;
+import info.magnolia.pages.app.action.SortComponentAction;
+import info.magnolia.pages.app.action.SortComponentActionDefinition;
 
+import javax.jcr.Item;
 import javax.jcr.Node;
 
+import java.util.Collections;
 import java.util.List;
 import info.magnolia.jcr.util.NodeUtil;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.jcr.RepositoryException;
-
+import javax.jcr.Session;
+import info.magnolia.context.MgnlContext;
 
 
 /**
  * Pastes one or more referenced components available in the clipboard to a user selected target area.
  */
 public class GatoPasteComponentAction extends PasteContentAction {
+    final EventBus eventBus;
+
     @Inject
     public GatoPasteComponentAction(GatoPasteComponentActionDefinition definition, JcrItemAdapter destination,
                                 @Named(SubAppEventBus.NAME) EventBus eventBus, UiContext uiContext, ComponentContentClipboard componentContentClipboard, SimpleTranslator i18n) {
         super(definition, destination, componentContentClipboard, uiContext, eventBus, i18n);
+        this.eventBus = eventBus;
     }
 
     @Override
@@ -44,7 +52,18 @@ public class GatoPasteComponentAction extends PasteContentAction {
             if(NodeUtil.isNodeType(node, "mgnl:component")) {
                 destinationPath = parent.getPath();
             }                        
-            List<AbstractElement> pastedItems = getContentClipboard().paste(new AreaElement(getDestination().getWorkspace(), destinationPath, null, null));
+            AreaElement areaElement = new AreaElement(getDestination().getWorkspace(), destinationPath, null, null);
+
+            List<AbstractElement> pastedItems = getContentClipboard().paste(areaElement);
+
+            Session session = MgnlContext.getJCRSession(areaElement.getWorkspace());
+            Collections.reverse(pastedItems); //preserve order if multiple components are being pasted
+            for (AbstractElement pastedNode : pastedItems) {
+                String nodePath = pastedNode.getPath();
+                Node sourceComponent = session.getNode(nodePath);
+                NodeUtil.orderAfter(sourceComponent, node.getName());
+            }
+            session.save();
 
             getUiContext().openNotification(MessageStyleTypeEnum.INFO, true, getI18n().translate("actions.pasteComponent.success", pastedItems.size()));
             getEventBus().fireEvent(new ContentChangedEvent(getDestination().getItemId()));
