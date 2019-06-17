06a7afef1aa7e8c46187f6cc0cd0fb0b77edc21e
Ross McEwen <ram146@txstate.edu>
2019-06-17 16:52:30 -0500

    Homepage: Update color picker colors

--- resources/gato-template-home/js/color-picker-config.js
+++ resources/gato-template-home/js/color-picker-config.js
@@ -1,7 +1,7 @@
 {
     "default": {
         "singleColor": true,
-        "colors": ["color1", "color2", "color3", "color4"]
+        "colors": ["color1", "color2", "color3", "color4", "color5", "color6", "color7"]
     },
     "iconText": {
         "singleColor": true,
@@ -9,34 +9,34 @@
     },
     "gradientButton": {
         "singleColor": true,
-        "colors": ["color1", "color2", "color3", "color4"]
+        "colors": ["color1", "color2", "color3", "color4", "color5", "color6", "color7"]
     },
     "rolloverButton": {
         "singleColor": true,
-        "colors": ["color1", "color2", "color3"]
+        "colors": ["color1", "color2", "color3", "color4", "color7"]
     },
     "reverseButton": {
         "singleColor": true,
-        "colors": ["color1", "color2", "color3"]
+        "colors": ["color1", "color2", "color3", "color4"]
     },
     "cardFilters": {
         "singleColor": true,
-        "colors": ["color1", "color2", "color3", "color4"]
+        "colors": ["color1", "color2", "color3", "color6"]
     },
     "imageCard": {
         "singleColor": true,
-        "colors": ["color1", "color2", "color3", "color4"]
+        "colors": ["color1", "color2", "color3", "color6"]
     },
     "slider": {
         "singleColor": false,
-        "colors": ["color1", "color2", "color3", "color4"]
+        "colors": ["color1", "color2", "color3", "color4", "color5", "color6", "color7"]
     },
     "txstateHomeTopSlider": {
         "singleColor": true,
-        "colors": ["color1", "color2", "color3", "color4"]
+        "colors": ["color1", "color2", "color3", "color4", "color5", "color6", "color7"]
     },
     "insetTextPattern": {
       "singleColor": true,
-      "colors": ["color1", "color2", "color3", "color4", "color5"]
+      "colors": ["color1", "color2", "color3", "color4", "color5", "color6", "color7", "color8"]
     }
 }
