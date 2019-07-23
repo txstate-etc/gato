a458a75248d4ef343135871c57b768c2b0c8c21d
Ross McEwen <ram146@txstate.edu>
2019-07-23 16:13:22 -0500

    Homepage: Minor style adjustments. Added background color to CTA: Icons

--- resources/gato-template-home/js/arrows.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-    $(document).ready(function () {
-        var arrow = $('.arrow-svg').first('svg').animate().move(150, 150);
-        console.log(arrowtest);
-    });
-})(jQuery);
-  
-
-
