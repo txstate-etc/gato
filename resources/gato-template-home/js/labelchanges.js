b1048bc180afb2bf1ade94878abc6fa55d7758ff
Anne Faber <anne.faber@txstate.edu>
2019-08-07 16:23:37 -0500

    Homepage: a few edit bar label changes

---
+++ resources/gato-template-home/js/labelchanges.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($) {
+  magnolialabelchange('.menubar .menu', '.mgnlEditor.mgnlPlaceholder', 'Add Header Links');
+  magnolialabelchange('.multilinks .editMultiLinks', '.mgnlEditor.mgnlEditorBar', 'Header Links');
+  magnolialabelchange('.mobilefirst_component_add', '.mgnlEditor.mgnlPlaceholder', "Add Section");
+});
--- resources/gato-template-home/js/standard.cjs
+++ resources/gato-template-home/js/standard.cjs
@@ -1,4 +1,5 @@
 /gato-lib/js/object-fit-images.min.js
 /gato-template-mobilefirst/js/menu.js
 /gato-lib/js/tabbed.js
+/gato-template-home/js/labelchanges.js
 /gato-template-home/js/hero.js
