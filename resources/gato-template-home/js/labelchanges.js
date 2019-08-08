2d6a0c99cb5d038d37e7aa2cacaae55fe3013275
Anne Faber <anne.faber@txstate.edu>
2019-08-08 13:15:46 -0500

    Homepage: change edit bar label for banner image

--- resources/gato-template-home/js/labelchanges.js
+++ resources/gato-template-home/js/labelchanges.js
@@ -2,4 +2,5 @@ jQuery(document).ready(function($) {
   magnolialabelchange('.menubar .menu', '.mgnlEditor.mgnlPlaceholder', 'Add Header Links');
   magnolialabelchange('.multilinks .editMultiLinks', '.mgnlEditor.mgnlEditorBar', 'Header Links');
   magnolialabelchange('.mobilefirst_component_add', '.mgnlEditor.mgnlPlaceholder', "Add Section");
+  magnolialabelchange('.addBannerImage', '.mgnlEditor.mgnlPlaceholder', 'Edit Banner Image');
 });
