74cf5f0dece7add810be84fa6d8fd5be28240907
Anne Faber <anne.faber@txstate.edu>
2019-09-13 14:00:26 -0500

    Accessibility: allow keyboard user to move through Audiences menu using arrow keys. Close Audiences menu with ESC key

--- resources/gato-template-home/js/homepage.js
+++ resources/gato-template-home/js/homepage.js
@@ -10,17 +10,55 @@ jQuery(document).ready(function($) {
   magnolialabelchange('.explore_add', '.mgnlEditor.mgnlPlaceholder', 'Add Card');
   magnolialabelchange('.explore_max', '.mgnlEditor.mgnlPlaceholder', 'Maximum cards added');
   
-  $('.top-bar .btn-audience').click(function() {
+  var $audienceButton = $('.top-bar .btn-audience');
+  
+  var closeAudiencePanel = function() {
+    $('.dropdown').removeClass("open")
+    $audienceButton.attr('aria-expanded', 'false')
+  }
+  $audienceButton.click(function() {
     var $dropdown = $('.dropdown')
     var button = $(this);
     if ($dropdown.hasClass('open')) {
-      $dropdown.removeClass('open');
-      button.attr('aria-expanded', 'false');
+      closeAudiencePanel();
     }
     else {
       $dropdown.addClass('open');
       button.attr('aria-expanded', 'true');
     }
   })
+  
+  $('.top-bar .btn-audience').on('keydown', function(e){
+    //close menu on ESC
+    if (e.keyCode == KeyCodes.ESCAPE && $('.dropdown').hasClass('open')) {
+      closeAudiencePanel();
+    }
+    if (e.keyCode == KeyCodes.DOWN || e.keyCode == KeyCodes.RIGHT) {
+      $('#audience-panel li a').first().focus();
+    }
+  })
+  $('#audience-panel').on('keydown', function(e) {
+    if (e.keyCode == KeyCodes.ESCAPE && $('.dropdown').hasClass('open')) {
+      closeAudiencePanel();
+      $audienceButton.focus();
+    }
+  })
+  $('#audience-panel li a').on('keydown', function(e) {
+    var $items = $('#audience-panel li a');
+    var $target = $(e.target);
+    if (e.keyCode == KeyCodes.DOWN || e.keyCode == KeyCodes.RIGHT) {
+      e.preventDefault();
+      var index = $items.index($target) + 1;
+      index = (index > ($items.length - 1)? 0 : index);
+      $items.get(index).focus();
+    }
+    else if (e.keyCode == KeyCodes.UP || e.keyCode == KeyCodes.LEFT) {
+      e.preventDefault();
+      var index = $items.index($target) - 1;
+      index = (index < 0 ? ($items.length - 1) : index);
+      $items.get(index).focus();
+    }
+  })
+  
 });
   
