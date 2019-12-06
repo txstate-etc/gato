f789e18096efdc779f8db2f462adc6307cc1f9a1
Anne Faber <anne.faber@txstate.edu>
2019-12-06 08:47:46 -0600

    Icon Picker: removed dependency on jquery ui dialog, improved appearance

--- resources/gato-template/js/icon-picker-modal.js
+++ resources/gato-template/js/icon-picker-modal.js
@@ -1,170 +1,142 @@
 function initIconPicker(def, node, el, tmpl) {
+  
+  var selectedIcon;
 
-    var selectedIcon;
-
-    //if there is an icon saved already, return its name
-    //if not, return the default graduation cap icon
-    function getSelectedIcon(){
-        var val = selectedIcon || $('input[type=hidden].icon').val();
-        return (val ? val : "fa-graduation-cap");
+  //if there is an icon saved already, return its name
+  //if not, return the default graduation cap icon
+  function getSelectedIcon(){
+    var val = selectedIcon || $('input[type=hidden].icon').val();
+    return (val ? val : "fa-graduation-cap");
+  }
+  
+  function updatePreview(icon){
+    $('.preview-icon').removeClass(function(index,css){
+      return (css.match(/(^|\s)fa\S+/g) || []).join(' ');
+    }).addClass('fa').addClass(icon);
+    $('.preview-name').text(icon);
+  }
+  
+  function save(){
+    var selected = $('input[type=radio][name=iconselect]:checked').val();
+    $('input[type=hidden].icon').val(selected).change();
+    selectedIcon = selected;
+    closeModal();
+  }
+  
+  var modalhtml = "";
+  modalhtml += '<div id="icon-modal" title="Select Icon">';
+  modalhtml += '<div tabindex="0" class="focusstart sr-only"></div>';
+  modalhtml += '<div class="icon-modal-content">'
+  modalhtml += '<button id="btnCloseIconModal"><i class="fa fa-close" aria-hidden="true"></i><span class="sr-only">Close Dialog</span></button>';
+  modalhtml += '<div class="search-wrapper">';
+  modalhtml += '<input id="search_for" placeholder="Search Icons"/>';
+  modalhtml += '<label for="search_for" class="fa fa-search" rel="tooltip" title="search"></label>';
+  modalhtml += '</div>';
+  modalhtml += '<fieldset>';
+  modalhtml += '<legend class="sr-only">Icons</legend>';
+  modalhtml += '   <div class="icon-picker-items"></div>';
+  modalhtml += '</fieldset>';
+  modalhtml += '<div id="no-icons">No Icons Found</div>';
+  modalhtml += '</div>';
+  modalhtml += '<div tabindex="0" class="focusend sr-only"></div>';
+  modalhtml += '</div>';
+  var html = '<span class="preview-icon"></span>';
+  html += '<button id="btnSelectIcon">SELECT NEW ICON</button>';
+  $(el).append(html);
+  $(el).closest('.dialog-content').append(modalhtml);
+  $('#icon-modal').hide();
+  
+  selectedIcon = getSelectedIcon();
+  updatePreview(selectedIcon);
+  
+  //open the dialog when the Select Icon button is clicked
+  $( "#btnSelectIcon" ).on( "click", function() {
+    openModal();
+  });
+  
+  $('#btnCloseIconModal').click(function() {
+    closeModal();
+  })
+  
+  $( "#icon-modal").on('keydown', function(e) {
+    e.stopPropagation();
+    if (e.key === 'Escape' || e.key ==='Esc') {
+      closeModal();
     }
-
-    //update the preview in the icon/text dialog
-    function updatePreview(icon){
-        $('.preview-icon').removeClass(function(index,css){
-            return (css.match(/(^|\s)fa\S+/g) || []).join(' ');
-        }).addClass('fa').addClass(icon);
-        $('.preview-name').text(icon);
+  })
+  
+  //trap focus in modal
+  $('.icon-modal-content').focusout(function (e) {
+    var tabbable = $('.icon-modal-content').find(':tabbable');
+    var first = tabbable.first();
+    var last = tabbable.last();
+    var targ = $(e.relatedTarget);
+    if (targ.is('.focusstart')) {
+      last.focus();
+    } else if (targ.is('.focusend')) {
+      first.focus();
     }
+  })
 
-    //create the jQuery dialog
-    function initializeDialog(){
-        $('#icon-modal').show();
-        dialog = $( "#icon-modal" ).dialog({
-            modal: true,
-            autoOpen: false,
-            width: 450,
-            height: 400,
-            maxHeight:450,
-            open: onOpen,
-            closeOnEscape: false,
-            close: function(event, ui){
-                $('#search_for').val('');
-                dialog.dialog("destroy").hide();
-            }
-        });
-        return dialog;
-    }
+  var openModal = function() {
+    $('#icon-modal').show();
+    createIconGrid(icons);
+    //show the current icon as selected
+    $('input[type=radio][name=iconselect][value='+ getSelectedIcon() +']').prop('checked', true);
+    
+    $('#no-icons').hide();
+    $('#btnCloseIconModal').focus();
+  }
 
-    function createInput(id) {
-        return '<label for="'+id+'" title="'+id+'" class="icon-picker-item">'+
-          '<input type="radio" name="iconselect" class="iconselect" value="'+id+'" id="'+id+'"/>'+
-          '<div><span class="fa ' + id +'"></span></div>'+
-        '</label>';
-    }
-
-    //save the icon selection when an icon is clicked
-    //update the preview in the picker when the user hovers over an icon
-    function attachIconEventHandlers(){
-        $('input[type=radio][name=iconselect]').off('change').on('change', function(event) {
-           $(this).prop("checked", true);
-           updatePreview($(this).val());
-           save();
-
-        });
-        $('.icon-picker-item').hover(function(){
-            var name = $(this).find('input').val();
-            setPickerPreview(name);
-        });
-    }
-
-    function createIconGrid(iconList){
-        var iconsHtml = "";
-        for (var i = 0; i < iconList.length; i++) {
-            iconsHtml += createInput(iconList[i]);
-        }
-        $('.icon-picker-items .icon-picker-item').remove();
-        $('.icon-picker-items').append(iconsHtml);
-        attachIconEventHandlers();
-        $('#no-icons').hide();
-    }
-
-    //The preview in the picker, not the one in the icon/text dialog
-    function setPickerPreview(icon){
-        $('.hover-icon').removeClass(function(index,css){
-            return (css.match(/(^|\s)fa\S+/g) || []).join(' ');
-        }).addClass('fa').addClass(icon);
-        $('.hover-icon-name').text(icon);
-    }
-
-    function onCancel(){
-        $('#search_for').val('');
-        $('#icon-modal').dialog('close');
-    }
-
-    //called when the icon-picker opens
-    function onOpen(){
-        createIconGrid(icons);
-        //show the current icon as selected
-        $('input[type=radio][name=iconselect][value='+ getSelectedIcon() +']').prop('checked', true);
-
-        //build the preview at the bottom of the icon picker
-        var previewHTML = '<div class="ui-dialog-buttonpane ui-widget-content ui-helper-clearfix">';
-        previewHTML += '<div class="preview">';
-        previewHTML += '<span class="hover-icon"></span>';
-        previewHTML += '<div class="hover-icon-name"></div>';
-        previewHTML += '</div>';
-        $('#icon-modal').after(previewHTML);
-        setPickerPreview(getSelectedIcon());
-
-        //get rid of the default close button and use a font awesome one instead
-        $('.ui-dialog-titlebar-close').hide().after('<div id="close-icon-picker"><i class="fa fa-close"></i></div>');
-
-        $('#close-icon-picker').click(onCancel);
-        //close dialog if they click off of it
-        $('.ui-widget-overlay').bind('click',function(){
-            onCancel();
-        });
-        $('.ui-dialog-titlebar, #search_for, .ui-dialog-buttonpane, .ui-widget-overlay').hover(function(){
-            setPickerPreview(getSelectedIcon());
-        });
-    }
-
-    //Save the icon selection in the hidden field
-    function save(){
-        var selected = $('input[type=radio][name=iconselect]:checked').val();
-        $('input[type=hidden].icon').val(selected).change();
-        selectedIcon = selected;
-        dialog.dialog("close");
-    }
-
-    var html = "";
-    html += '<div id="icon-modal" title="Select Icon">';
-    html += '<div class="search-wrapper">';
-    html += '<input id="search_for" placeholder="Search Icons"/>';
-    html += '<label for="search_for" class="fa fa-search" rel="tooltip" title="search"></label>';
-    html += '</div>';
-    html += '   <div class="icon-picker-items">';
-
-    html += '</div>';
-    html += '<div id="no-icons">No Icons Found</div>';
-    html += '</div>';
-    html += '<span class="preview-icon"></span>';
-    html += '<button id="btnSelectIcon">Select New Icon</button>';
-    $(el).append(html);
+  var closeModal = function() {
+    $('#search_for').val('');
     $('#icon-modal').hide();
+    $('#btnSelectIcon').focus();
+  }
+  
+  function attachIconEventHandlers(){
+    $('input[type=radio][name=iconselect]').off('change').on('change', function(event) {
+       $(this).prop("checked", true);
+       updatePreview($(this).val());
+       save();
 
-    //if there is an icon saved already, display it in the preview
-    //if not, display the default icon
-    selectedIcon = getSelectedIcon();
-    updatePreview(selectedIcon);
-
-    var dialog;
-
-    //open the dialog when the Select Icon button is clicked
-    $( "#btnSelectIcon" ).button().on( "click", function() {
-      dialog = initializeDialog();
-      dialog.dialog("open");
     });
+  }
+  
+  function createInput(id) {
+    return '<label for="'+id+'" title="'+id+'" class="icon-picker-item">'+
+      '<input type="radio" name="iconselect" class="iconselect" value="'+id+'" id="'+id+'"/>'+
+      '<div><span class="fa fa-fw ' + id +'"></span><span class="sr-only">' + id.substring(3) + '</span></div>'+
+    '</label>';
+  }
+  
+  var createIconGrid = function(iconList) {
+    var html = "";
+    for (var i=0; i< iconList.length; i++) {
+      html += createInput(iconList[i]);
+    }
+    $('.icon-picker-items .icon-picker-item').remove();
+    $('.icon-picker-items').append(html);
+    attachIconEventHandlers();
+  }
+  
+  //narrow down the displayed icons as the user types
+  $('#search_for').on("keyup", function(){
+      var searchVal = $(this).val();
+      function isSubstring(iconName){
+          return iconName.indexOf(searchVal) > -1;
+      }
+      var filteredIcons = icons.filter(isSubstring);
+      createIconGrid(filteredIcons);
+      //if no icons match the search term, display a message
+      if(filteredIcons.length < 1){
+          $('#no-icons').show();
+      }
+      else{
+          $('#no-icons').hide();
+      }
+  });
 
-    //narrow down the displayed icons as the user types
-    $('#search_for').on("keyup", function(){
-        var searchVal = $(this).val();
-        function isSubstring(iconName){
-            return iconName.indexOf(searchVal) > -1;
-        }
-        var filteredIcons = icons.filter(isSubstring);
-        createIconGrid(filteredIcons);
-        //if no icons match the search term, display a message
-        //and hide the preview
-        if(filteredIcons.length < 1){
-            $('#no-icons').show();
-            $('.preview').css('visibility', 'hidden');
-        }
-        else{
-            $('#no-icons').hide();
-            setPickerPreview(getSelectedIcon());
-            $('.preview').css('visibility', 'visible');
-        }
-    });
 }
+
+
