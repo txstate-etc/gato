ee4e25514f754590cfae1652d0e81248916aab66
Anne Faber <anne.faber@txstate.edu>
2020-02-14 13:31:16 -0600

    Add red option to Admissions Facts/Figures prebuilt sections

--- resources/gato-template-admissions/js/color-picker-config.js
+++ resources/gato-template-admissions/js/color-picker-config.js
@@ -41,6 +41,6 @@
     },
     "factsFigures": {
       "singleColor": true,
-      "colors": ["color1", "color2", "color3", "color4", "color5", "color6", "color7", "color11"]
+      "colors": ["color1", "color2", "color3", "color4", "color5", "color6", "color7", "color11", "color12"]
     }
 }
