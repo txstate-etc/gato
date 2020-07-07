fb05fe4111dc8c4004e854514fff4f54d9c81e3a
Anne Faber <anne.faber@txstate.edu>
2020-07-07 12:28:47 -0500

    Slider: add back code that truncates long captions

--- resources/gato-component-feature/js/feature.js
+++ resources/gato-component-feature/js/feature.js
@@ -53,7 +53,51 @@ jQuery(document).ready(function($) {
       $slideshow.find(".slides .moving-image").each(function(index,slide){
         $(slide).find('.image-container').height($slideshow.width() * 9.0 / 16.0)
       })
+
+      //Limit text on image slides to two lines.  If the text goes over 2 lines, truncate it and
+      //add an ellipsis.  This code calculates a maximum height based on the line height and removes
+      //one word at a time until the content is truncated enough to fit on two lines.  It also removes
+      //some special characters from the end of words so you don't have something like
+      //question?... or fire!...  Once the position of the caption is relative (slider is in a smaller area),
+      //the entire text is shown in the caption area under the image
+      $('.gato-slideshow .slides .slide .caption p').each(function() {
+        var description = $(this)
+        var originalText = description.data('orig-text').replace(/<br\/?>/i,'').replace(/<[^>]*\s[^>]*>/, '').replace(/\s+(<\/[^>]*>)/, '$1').replace(/(<[^\/][^>]*>)\s+/, '$1');
+        var caption = description.closest('.caption');
+        if(caption.css('position') == 'absolute' && !description.data('skip-truncation')){
+          var maxLineHeight = Math.round(2 * parseFloat(description.css('line-height')))
+          //if this has been resized, we need to reset the original text
+          description.html(originalText)
+          var height = Math.round(description.height())
+          //in this case, it is already small enough
+          if (height <= maxLineHeight) return
+          //remove one word at a time from the end until it fits
+          var wordsToRemove = 0
+          var words = originalText.split(" ")
+          while (height > maxLineHeight) {
+            var newText = words.slice(0, words.length-wordsToRemove).join(' ') + "..."
+            description.html(newText)
+            height = Math.round(description.height())
+            wordsToRemove ++;
+          }
+          //If these characters appear right before the ellipsis it will look strange, so remove them
+          var uglyLastCharacters = [ ' ', '\u3000', ',', ';', '.', '!', '?' ]
+          if (newText.length > 4){
+            var lastChar = newText.charAt(newText.length-4)
+            var index = jQuery.inArray(lastChar, uglyLastCharacters);
+            while(index > -1 && newText.length > 4){
+              newText = newText.slice(0,newText.length-4) + newText.slice(newText.length-3);
+              lastChar = newText.charAt(newText.length-4);
+              index = jQuery.inArray(lastChar, uglyLastCharacters);
+            }
+          }
+          description.html(newText);
+        }
+        else{
+          //The caption is beneath the image, no need to truncate
+          description.html(originalText);
+        }
+      })
     })
   });
-
 });
