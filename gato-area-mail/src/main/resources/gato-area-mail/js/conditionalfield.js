a4401616ba83c09980fb613b80fb65f97c862360
Anne Faber <anne.faber@txstate.edu>
2020-08-04 12:02:31 -0500

    Don't submit conditional form fields that are not part of the visible form

--- gato-area-mail/src/main/resources/gato-area-mail/js/conditionalfield.js
+++ gato-area-mail/src/main/resources/gato-area-mail/js/conditionalfield.js
@@ -1,11 +1,29 @@
 jQuery(document).ready(function($) {
   $('.formelement.conditional').each(function() {
     var $field = $(this)
+    $field.find('.conditional-questions input').each(function() {
+      var $input = $(this)
+      removeName($input)
+    })
     $(this).find('.choice').change(function(e) {
       var $selected = $(e.target)
       var answergroupid = $selected.data('answergroup')
       $('.conditional-answer').removeClass('active')
       $('#' + answergroupid).addClass('active')
+      $('#' + answergroupid).find('input').each(function() {
+        replaceName($(this))
+      })
     })
   })
+
+  function removeName($input) {
+    var name = $input.attr('name')
+    $input.attr('data-name', name)
+    $input.removeAttr('name')
+  }
+
+  function replaceName($input) {
+    var name = $input.attr('data-name')
+    $input.attr('name', name)
+  }
 })
