f8933535e9797f7d899c8952fcad731b2d0cf51e
Anne Faber <anne.faber@txstate.edu>
2021-06-25 13:23:57 -0500

    convert outgoing clicks code to jQuery

--- resources/gato-template/js/outgoing-clicks.js
+++ resources/gato-template/js/outgoing-clicks.js
@@ -1,31 +1,28 @@
 // generate Google Analytics events for link clicks
-document.observe('dom:loaded', function() {
-  $$('a').each( function(item) {
-    item.observe('click', function() {
-      var linkAddress = item.href;
-      var linkName = item.text;
-      var thisPageAddress = window.location;
-      var thisPageTitle = document.title;
-
-      if (typeof(destroyerGlobalPageTracker) != 'undefined') {
-        ga(destroyerGlobalPageTracker.name + '.send', {
-          'hitType': 'event',
-          'eventCategory': 'Links',
-          'eventAction': thisPageTitle + " <" + thisPageAddress + ">",
-          'eventLabel': linkName + " <" + linkAddress + ">",
-          'transport': 'beacon'
-        });
-      }
-
-      if (typeof(destroyerSitePageTracker) != 'undefined') {
-        ga(destroyerSitePageTracker.name + '.send', {
-          'hitType': 'event',
-          'eventCategory': 'Links',
-          'eventAction': thisPageTitle + " <" + thisPageAddress + ">",
-          'eventLabel': linkName + " <" + linkAddress + ">",
-          'transport': 'beacon'
-        });
-      }
-    });
-  });
+jQuery(document).ready(function($) {
+  $('a').on('click', function() {
+    var link = $(this)
+    var linkAddress = link.attr('href')
+    var linkName = link.text()
+    var thisPageAddress = window.location
+    var thisPageTitle = document.title
+    if (typeof(destroyerGlobalPageTracker) != 'undefined') {
+      ga(destroyerGlobalPageTracker.name + '.send', {
+        'hitType': 'event',
+        'eventCategory': 'Links',
+        'eventAction': thisPageTitle + " <" + thisPageAddress + ">",
+        'eventLabel': linkName + " <" + linkAddress + ">",
+        'transport': 'beacon'
+      })
+    }
+    if (typeof(destroyerSitePageTracker) != 'undefined') {
+      ga(destroyerSitePageTracker.name + '.send', {
+        'hitType': 'event',
+        'eventCategory': 'Links',
+        'eventAction': thisPageTitle + " <" + thisPageAddress + ">",
+        'eventLabel': linkName + " <" + linkAddress + ">",
+        'transport': 'beacon'
+      });
+    }
+  })
 });
