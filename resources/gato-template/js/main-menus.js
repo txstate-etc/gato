19df67412615adecbace1db42b3f066d1ea18e40
Anne Faber <anne.faber@txstate.edu>
2021-07-05 10:06:10 -0500

    Refactor 2015 menu code to use jQuery instead of Prototype

--- resources/gato-template/js/main-menus.js
+++ resources/gato-template/js/main-menus.js
@@ -1,93 +1,94 @@
-function ddmenu_hideall() {
-	if (typeof ddmenu_hideall.dd_divs == 'undefined') {
-		ddmenu_hideall.dd_divs = new Array();
-		$$('div.ddmenu-menubaritem').each( function(item) {
-			ddmenu_hideall.dd_divs.push(item);
-		});
+jQuery(document).ready(function($) {
+
+	function menushown(bar) {
+		return $(bar).hasClass('ddmenu-visible')
 	}
-	var mydivs = ddmenu_hideall.dd_divs;
-	var len = mydivs.length;
-	for (i = 0; i < len; i++) {
-		hidemenu(mydivs[i]);
+
+	function ddmenu_hideall() {
+		$('div.ddmenu-menubaritem').removeClass('ddmenu-visible')
 	}
-}
 
-function showmenu(bar) {
-	if (!bar.dd_shown) {
-		ddmenu_hideall();
-		bar.addClassName('ddmenu-visible');
-		bar.dd_shown = true;
+	function showmenu(bar) {
+		if (!menushown(bar)) {
+			ddmenu_hideall()
+			$(bar).addClass('ddmenu-visible')
+		}
 	}
-}
 
-function hidemenu(bar) {
-	if (bar.dd_shown) {
-		bar.removeClassName('ddmenu-visible');
-		bar.dd_shown = false;
+	function hidemenu(bar) {
+		if (menushown(bar)) {
+			$(bar).removeClass('ddmenu-visible');
+		}
 	}
-}
 
-function ddmenu_over(bar) {
-	if (bar && bar.dd_timer) clearTimeout(bar.dd_timer);
-	if (bar && !bar.dd_shown) bar.dd_timer = setTimeout("showmenu($('"+bar.id+"'))", 100);
-}
+	function ddmenu_over(bar) {
+		if (bar && bar.dd_timer) clearTimeout(bar.dd_timer);
+		if (bar && !menushown(bar)) {
+			bar.dd_timer = setTimeout(function(){
+				showmenu(bar)
+			}, 100);
+		}
+	}
 
-function ddmenu_out(bar) {
-	if (bar && bar.dd_timer) clearTimeout(bar.dd_timer);
-	if (bar && bar.dd_shown) bar.dd_timer = setTimeout("hidemenu($('"+bar.id+"'))", 250);
-}
+	function ddmenu_out(bar) {
+		if (bar && bar.dd_timer) clearTimeout(bar.dd_timer)
+		if (bar && menushown(bar)) {
+			bar.dd_timer = setTimeout(function() {
+				hidemenu(bar)
+			}, 250)
+		}
+	}
 
-Event.observe(document, 'dom:loaded', function (event) {
-	var count = 0;
-	$$('div.ddmenu-menubaritem').each( function (item) {
-		item.id = 'ddmenu-'+count;
-		count++;
+	var count = 0
+	$('div.ddmenu-menubaritem').each(function() {
+		var item = $(this)
+		item.attr('id', 'ddmenu-'+count)
+		count++
 
-		var menuwrap = item.down('.ddmenu-menu');
-		item.observe('touchstart', function (event) {
-			if (!item.dd_shown && menuwrap) {
-				showmenu(item);
-				event.stop();
+		var menuwrap = item.find('.ddmenu-menu')
+		item.on('touchstart', function (e) {
+			if (!item.hasClass('ddmenu-visible') && menuwrap.length) {
+				showmenu(item)
+				e.stopPropagation()
 			}
-		});
-		var menubg = item.up(1);
-		if (menubg) {
-			menubg.observe('touchstart', function(event) {
-				if (!item.nohide) hidemenu(item);
-			});
-		}
-		if (menuwrap) {
-			menuwrap.observe('touchstart', function(event) {
-				item.nohide = true;
-			});
-			menuwrap.observe('touchend', function(event) {
-				item.nohide = false;
-			});
-		}
-		item.observe('mouseover', function (event) {
-			ddmenu_over(this);
-			event.stop();
-		});
-		item.observe('mouseout', function (event) {
-			ddmenu_out(this);
-			event.stop();
-		});
-	});
-	// add support for tabbing through links with the keyboard
-	$$('.ddmenu-menubaritem a').each(function(item) {
-		item.observe('focus', function (event) {
-			ddmenu_over(this.up('div.ddmenu-menubaritem'));
-			event.stop();
-		});
-		item.observe('blur', function (event) {
-			ddmenu_out(this.up('div.ddmenu-menubaritem'));
-			event.stop();
-		});
-	});
-});
+		})
 
-Event.observe(window, 'load', function (event) {
-	$$('div.ddmenu-menubaritem').each( function (item) {
-		if (item.down('img')) item.down('img').setStyle({width: item.down('img').getWidth()+'px'});
-	});
+		var menubg = item.closest('.ddmenu-bg')
+		if (menubg.length) {
+			menubg.on('touchstart', function(e) {
+				if (!item.data(nohide)) hidemenu(item);
+			});
+		}
+
+		if (menuwrap.length) {
+			menuwrap.on('touchstart', function(e) {
+				item.data('nohide', true)
+			})
+			menuwrap.on('touchend', function(e) {
+				item.data('nohide', false)
+			})
+		}
+
+		item.on('mouseover', function (e) {
+			ddmenu_over(this)
+			e.stopPropagation()
+		});
+		item.on('mouseout', function (e) {
+			ddmenu_out(this);
+			e.stopPropagation()
+		});
+	})
+	
+	// add support for tabbing through links with the keyboard
+	$('.ddmenu-menubaritem a').each(function() {
+		var item = $(this)
+		item.on('focus', function(e) {
+			ddmenu_over(this.closest('div.ddmenu-menubaritem'))
+			e.stopPropagation()
+		})
+		item.on('blur', function(e) {
+			ddmenu_out(this.closest('div.ddmenu-menubaritem'))
+			e.stopPropagation()
+		})
+	})
 });
